304fe7e21946ac1aea0996b32f07aa7021d01d3a
Maxwell Sayles <maxwellsayles@gmail.com>
2016-12-14 21:23:56 -0800

    annoyingly mechanical

--- day1.scala
+++ day1.scala
@@ -1,5 +1,6 @@
 val input = {
   val txt = "R3, R1, R4, L4, R3, R1, R1, L3, L5, L5, L3, R1, R4, L2, L1, R3, L3, R2, R1, R1, L5, L2, L1, R2, L4, R1, L2, L4, R2, R2, L2, L4, L3, R1, R4, R3, L1, R1, L5, R4, L2, R185, L2, R4, R49, L3, L4, R5, R1, R1, L1, L1, R2, L1, L4, R4, R5, R4, L3, L5, R1, R71, L1, R1, R186, L5, L2, R5, R4, R1, L5, L2, R3, R2, R5, R5, R4, R1, R4, R2, L1, R4, L1, L4, L5, L4, R4, R5, R1, L2, L4, L1, L5, L3, L5, R2, L5, R4, L4, R3, R3, R1, R4, L1, L2, R2, L1, R4, R2, R2, R5, R2, R5, L1, R1, L4, R5, R4, R2, R4, L5, R3, R2, R5, R3, L3, L5, L4, L3, L2, L2, R3, R2, L1, L1, L5, R1, L3, R3, R4, R5, L3, L5, R1, L3, L5, L5, L2, R1, L3, L1, L3, R4, L1, R3, L2, L2, R3, R3, R4, R4, R1, L4, R1, L5"
+  // val txt = "R8, R4, R4, R8"
   val xs = txt.split(", ")
   xs.map(_.head) zip xs.map(_.tail.toInt)
 }
@@ -23,8 +24,25 @@ def step: ((Direction, Point), (Char, Int)) => (Direction, Point) = {
 }
 
 val solution = input.foldLeft((Direction(0, 1), Point(0, 0)))(step)
-println(solution)
-
 val p = solution._2
 println(p.x.abs + p.y.abs)
+
+def moveTowards(p: Point, q: Point): Point = {
+  if (p.x == q.x && p.y < q.y) Point(p.x, p.y + 1)
+  else if (p.x == q.x && p.y > q.y) Point(p.x, p.y - 1)
+  else if (p.x < q.x) Point(p.x + 1, p.y)
+  else Point(p.x - 1, p.y)
+}
+
+def interp(p: Point, xs: List[Point], acc: Set[Point]): Point = {
+  if (acc.contains(p)) p else {
+    val hd :: tl = xs
+    if (p == hd) interp(p, tl, acc)
+    else interp(moveTowards(p, hd), xs, acc + p)
+  }
+}
+
+val visits = input.scanLeft((Direction(0, 1), Point(0, 0)))(step).map(_._2)
+val p2 = interp(Point(0, 0), visits.toList.tail, Set())
+println(p2.x.abs + p2.y.abs)
 
