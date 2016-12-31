52b048e1b3908efc4c957e4c7e8c16ce901d2408
Maxwell Sayles <maxwellsayles@gmail.com>
2016-12-30 20:59:33 -0800

    part 2 of day 4. not hard

--- day4.scala
+++ day4.scala
@@ -1,6 +1,6 @@
 val input = io.Source.fromFile("day4.txt").mkString.lines.toList
 
-def extract(s: String): Option[Int] = {
+def extract(s: String): Option[(String, Int)] = {
   val pattern = """(.*)-(\d*)\[(.*)\]""".r
   s match {
     case pattern(s, secId, top5) => {
@@ -13,15 +13,28 @@ def extract(s: String): Option[Int] = {
         .take(5)
         .map(_._1)
         .mkString
-      if (s2 == top5) Some(secId.toInt) else None
+      if (s2 == top5) Some((s, secId.toInt)) else None
     }
   }
 }
+
+val xs = input.map(extract).flatten
 
 println(extract("aaaaa-bbb-z-y-x-123[abxyz]"))
 println(extract("a-b-c-d-e-f-g-h-987[abcde]"))
 println(extract("not-a-real-room-404[oarel]"))
 println(extract("totally-real-room-200[decoy]"))
+
+println("Part 1: " + xs.map(_._2).sum)
 println()
 
-println(input.map(extract).flatten.sum)
+def rotate(c: Char, x: Int): Char = 
+  if (c == '-') ' ' else ((((c - 'a') + x) % 26) + 'a').toChar
+
+def rotate(s: String, x: Int): String =
+  s.map(rotate(_, x))
+
+println(rotate("qzmt-zixmtkozy-ivhz", 343))
+
+val sol = xs.filter(v => rotate(v._1, v._2) == "northpole object storage").head
+println("Part 2: " + sol._2)
