0618eb55de19736b025cd7086a0dad1ef7fbab57
Maxwell Sayles <maxwellsayles@gmail.com>
2018-07-12 22:45:40 -0700

    ugh. scala is pretty terrible at folding over a large sequence and building an immutable map. it is even pretty bad given a mutable map and converting this to an immutable map. and then it is bad at tail recursion iterating over nodes in the circular list. anyway, this works, but needs extra memory.

--- day19.scala
+++ day19.scala
@@ -1,4 +1,7 @@
-val input = 3017957
+import scala.collection.mutable.HashMap
+
+val input: Int = 3017957
+//val input: Int = 5
 
 def solve(i: Int, j: Int, k: Int): Int = {
   if (i == j) i
@@ -10,3 +13,43 @@ def solve(i: Int, j: Int, k: Int): Int = {
 }
 
 println(solve(1, input, 1))
+
+case class State(
+  nodes: Map[Int, (Int, Int)],
+  taker: Int,
+  giver: Int,
+  dist: Int
+) {
+  def isFinished: Boolean = nodes.size == 1
+  def result: Int = nodes.keys.head
+  def size = nodes.size
+  def nextTaker: State = State(nodes, nodes(taker)._2, giver, dist - 1)
+  def nextGiver: State = State(nodes, taker, nodes(giver)._2, dist + 1)
+  def removeGiver: State = {
+    val (before, after) = nodes(giver)
+    val newAfter = (before, nodes(after)._2)
+    val newBefore = (nodes(before)._1, after)
+    val newNodes = nodes - giver + (after -> newAfter) + (before -> newBefore)
+    State(newNodes, taker, after, dist)
+  }
+}
+
+def advanceGiverToDist(state: State, targetDist: Int): State = {
+  if (state.dist == targetDist) state
+  else advanceGiverToDist(state.nextGiver, targetDist)
+}
+
+val initNodes = {
+  val m = HashMap[Int, (Int, Int)]()
+  0.until(input).foreach(x => m += x -> ((x + input - 1) % input, (x + 1) % input))
+  m.toMap
+}
+
+val initState = State(initNodes, 0, 0, 0)
+
+def solve2(state: State): Int = {
+  if (state.isFinished) state.result
+  else solve2(advanceGiverToDist(state, state.size / 2).removeGiver.nextTaker)
+}
+
+println(solve2(initState) + 1)
