065967e1b7db5b70d9a96e22887df6b214b2eaf0
Maxwell Sayles <maxwellsayles@gmail.com>
2018-07-13 22:18:08 -0700

    no need for a doubly linked list, we just need the previous node

--- day19.scala
+++ day19.scala
@@ -15,22 +15,21 @@ def solve(i: Int, j: Int, k: Int): Int = {
 println(solve(1, input, 1))
 
 case class State(
-  nodes: Map[Int, (Int, Int)],
+  nodes: Map[Int, Int],
   taker: Int,
+  giverPrev: Int,
   giver: Int,
   dist: Int
 ) {
   def isFinished: Boolean = nodes.size == 1
   def result: Int = nodes.keys.head
   def size = nodes.size
-  def nextTaker: State = State(nodes, nodes(taker)._2, giver, dist - 1)
-  def nextGiver: State = State(nodes, taker, nodes(giver)._2, dist + 1)
+  def nextTaker: State = State(nodes, nodes(taker), giverPrev, giver, dist - 1)
+  def nextGiver: State = State(nodes, taker, giver, nodes(giver), dist + 1)
   def removeGiver: State = {
-    val (before, after) = nodes(giver)
-    val newAfter = (before, nodes(after)._2)
-    val newBefore = (nodes(before)._1, after)
-    val newNodes = nodes - giver + (after -> newAfter) + (before -> newBefore)
-    State(newNodes, taker, after, dist)
+    val giverNext = nodes(giver)
+    val newNodes = nodes - giver + (giverPrev -> giverNext)
+    State(newNodes, taker, giverPrev, giverNext, dist)
   }
 }
 
@@ -40,12 +39,12 @@ def advanceGiverToDist(state: State, targetDist: Int): State = {
 }
 
 val initNodes = {
-  val m = HashMap[Int, (Int, Int)]()
-  0.until(input).foreach(x => m += x -> ((x + input - 1) % input, (x + 1) % input))
+  val m = HashMap[Int, Int]()
+  0.until(input).foreach(x => m += x -> (x + 1) % input)
   m.toMap
 }
 
-val initState = State(initNodes, 0, 0, 0)
+val initState = State(initNodes, 0, 0, 1, 1)
 
 def solve2(state: State): Int = {
   if (state.isFinished) state.result
