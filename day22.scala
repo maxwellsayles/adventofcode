8ea41f8bfcbbee204ae26eb49e9ecbd914efa4d0
Maxwell Sayles <maxwellsayles@gmail.com>
2018-08-11 21:31:32 -0700

    rename and clean up for original solution progressing to p2

--- day22.scala
+++ day22.scala
@@ -1,25 +1,20 @@
 val input = io.Source.fromFile("day22.txt").mkString.lines
 
-case class Stat(size: Int, used: Int)
+case class NodeStat(size: Int, used: Int)
 
 val dfRegex = """/dev/grid/node-x(\d+)-y(\d+)\s+(\d+)T\s+(\d+)T\s+(\d+)T.*""".r
-val grid = input.foldLeft(Map[(Int, Int), Stat]())({
+val initGrid = input.foldLeft(Map[(Int, Int), NodeStat]())({
   case (acc, dfRegex(x, y, size, used, _)) => {
-    acc + ((x.toInt, y.toInt) -> Stat(size.toInt, used.toInt))
+    acc + ((x.toInt, y.toInt) -> NodeStat(size.toInt, used.toInt))
   }
 })
 
-val maxx = grid.keys.map(_._1).max
-val maxy = grid.keys.map(_._2).max
+val maxx = initGrid.keys.map(_._1).max
+val maxy = initGrid.keys.map(_._2).max
 
 def isViable(sx: Int, sy: Int, tx: Int, ty: Int): Boolean = {
-    val Stat(tsize, tused) = grid(tx, ty)
-    tused + grid(sx, sy).used <= tsize
-}
-
-def isNeighbor(sx: Int, sy: Int, tx: Int, ty: Int): Boolean = {
-  (((sx == tx + 1 || sx == tx - 1) && sy == ty) ||
-    ((sy == ty + 1 || sy == ty - 1) && sx == tx))
+    val NodeStat(tsize, tused) = initGrid(tx, ty)
+    tused + initGrid(sx, sy).used <= tsize
 }
 
 val pairs = for (
@@ -27,13 +22,11 @@ val pairs = for (
   sy <- 0 to maxy;
   tx <- 0 to maxx;
   ty <- 0 to maxy;
-  if grid(sx, sy).used != 0 && !(sx == tx && sy == ty) && isViable(sx, sy, tx, ty) && isNeighbor(sx, sy, tx, ty))
+  if initGrid(sx, sy).used != 0 && !(sx == tx && sy == ty) && isViable(sx, sy, tx, ty))
     yield (sx, sy, tx, ty)
 
-//println(pairs.size)
-
-pairs.foreach(println)
-
+println
+println(pairs.size)
 
 // Track payload and empty block.
 // Move neighbors into empty block.
