afd6706edeee995a10bf0dd79188acf262869f76
Maxwell Sayles <maxwellsayles@gmail.com>
2018-09-18 21:55:52 -0700

    lots of subtle things there, but not difficult

--- day2.hs
+++ day2.hs
@@ -1,5 +1,18 @@
 import Control.Applicative
 import Control.Arrow
+import Data.List
+import Prelude hiding (divMod)
+
+divMod :: Int -> Int -> (Int, Int)
+divMod x y =
+  if x > y then (x `div` y, x `mod` y) else (y `div` x, y `mod` x)
+
+special :: [Int] -> Int
+special xs =
+  head $
+  map fst $
+  filter ((== 0) . snd) $
+  [divMod x y | (x:ys) <- tails xs, y <- ys]
 
 main :: IO ()
 main = do
@@ -7,3 +20,6 @@ main = do
 
   -- part 1
   print $ sum $ map (uncurry (-) . (maximum &&& minimum)) input
+
+  -- part 2
+  print $ sum $ map special input
