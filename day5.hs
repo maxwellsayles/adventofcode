c080365d0d13779ccd7228a203d5743b5983b7e7
Maxwell Sayles <maxwellsayles@gmail.com>
2018-09-21 23:05:19 -0700

    still ezpz but gotta be careful of laziness

--- day5.hs
+++ day5.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE BangPatterns #-}
 import Control.Applicative
 import qualified Data.IntMap as IM
 
@@ -7,8 +8,18 @@ stepCount m ip count =
     Nothing -> count
     Just steps -> stepCount (IM.update (Just . succ) ip m) (ip + steps) (count + 1)
 
+stepCount2 :: IM.IntMap Int -> Int -> Int -> Int
+stepCount2 m ip !count =
+  case IM.lookup ip m of
+    Nothing -> count
+    Just steps ->
+      if steps < 3
+      then stepCount2 (IM.update (Just . succ) ip m) (ip + steps) (count + 1)
+      else stepCount2 (IM.update (Just . pred) ip m) (ip + steps) (count + 1)
+
 main :: IO ()
 main = do
   input <- map (read :: String -> Int) . lines <$> readFile "day5.txt"
   let initSteps = IM.fromList $ zip [0..] input
   print $ stepCount initSteps 0 0
+  print $ stepCount2 initSteps 0 0
