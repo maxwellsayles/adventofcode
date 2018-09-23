7f85b5e98c426dd58fdb3227230d0d1dc715d316
Maxwell Sayles <maxwellsayles@gmail.com>
2018-09-22 22:46:22 -0700

    still easy

--- day6.hs
+++ day6.hs
@@ -1,6 +1,8 @@
+{-# LANGUAGE BangPatterns #-}
 import Control.Applicative
 import Data.List
 import qualified Data.Set as S
+import qualified Data.Map as M
 
 step :: [Int] -> [Int]
 step xs =
@@ -17,8 +19,15 @@ solve visited xs =
   then S.size visited
   else solve (S.insert xs visited) (step xs)
 
+solve2 :: M.Map [Int] Int -> [Int] -> Int -> Int
+solve2 visited xs !i =
+  case M.lookup xs visited of
+    Just n -> i - n
+    Nothing -> solve2 (M.insert xs i visited) (step xs) (i + 1)
+
 main :: IO ()
 main = do
   input <- map (read :: String -> Int) . words <$> readFile "day6.txt"
   print input
   print $ solve S.empty input
+  print $ solve2 M.empty input 0
