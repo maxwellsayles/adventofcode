9d8bee632625318f6bf7c08f195e4b638ccfd443
Maxwell Sayles <maxwellsayles@gmail.com>
2018-10-02 22:07:48 -0700

    big jump from p1 to p2, and actually some assumptions about the state of the puzzle. seems a bit early for a stumper, but not too bad considering the big space of puzzles

--- day7.hs
+++ day7.hs
@@ -1,9 +1,13 @@
 {-# LANGUAGE OverloadedStrings #-}
 
 import Control.Applicative
-import Data.Set (Set)
+import Control.Arrow
+import Data.Function (on)
+import Data.List (groupBy, sortBy)
+import Data.Ord (comparing)
 import Data.Text (Text)
 
+import qualified Data.Map as M
 import qualified Data.Set as S
 import qualified Data.Text as T hiding (concatMap)
 
@@ -17,11 +21,65 @@ supports l =
      then []
      else T.splitOn ", " (T.unwords (drop 3 ws))
 
-supported :: [Text] -> Set Text
+supported :: [Text] -> S.Set Text
 supported = S.fromList . concatMap supports
+
+getName :: Text -> Text
+getName = head . T.words
+
+getWeight :: Text -> Int
+getWeight = read . T.unpack . T.tail . T.init . (!! 1) . T.words
+
+nameToWeight :: [Text] -> M.Map Text Int
+nameToWeight = M.fromList . map (getName &&& getWeight)
+
+nameToSupports :: [Text] -> M.Map Text [Text]
+nameToSupports = M.fromList . map (getName &&& supports)
+
+nameToStackWeight :: M.Map Text [Text]
+                  -> M.Map Text Int
+                  -> Text
+                  -> M.Map Text Int
+nameToStackWeight supportMap weightMap root =
+  helper root M.empty
+  where
+    helper name acc =
+      let supports = supportMap M.! name
+          weight = weightMap M.! name
+          acc' = foldr helper acc supports
+          childStackWeights = map (acc' M.!) supports
+          stackWeight = weight + sum childStackWeights
+      in M.insert name stackWeight acc'
+
+maybeImbalanced :: M.Map Text [Text]
+                -> M.Map Text Int
+                -> Text
+                -> Maybe (Text, Int)
+maybeImbalanced supportMap stackWeightMap name =
+  let supports = supportMap M.! name
+      groups = sortBy (comparing length) $
+               groupBy ((==) `on` snd) $
+               sortBy (comparing snd) $
+               map (id &&& (stackWeightMap M.!)) supports
+  in case length groups of
+    1 -> Nothing
+    2 -> Just (fst $ head $ groups !! 0, snd $ head $ groups !! 1)
+
+solve2 supportMap weightMap stackWeightMap root =
+  helper root 0
+  where
+    helper name v =
+      case maybeImbalanced supportMap stackWeightMap name of
+        Nothing -> (weightMap M.! name) + v - (stackWeightMap M.! name)
+        Just (name, v') -> helper name v'
 
 main :: IO ()
 main = do
   input <- T.lines . T.pack <$> readFile "day7.txt"
   let names = S.fromList $ getNames input
-  putStrLn $ T.unpack $ head $ S.toList $ S.difference names (supported input)
+      root = head $ S.toList $ S.difference names (supported input)
+      supportMap = nameToSupports input
+      weightMap = nameToWeight input
+      stackWeightMap = nameToStackWeight supportMap weightMap root
+  putStrLn $ T.unpack root
+  print $ solve2 supportMap weightMap stackWeightMap root
