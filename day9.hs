47ddc0e512726e975770ff9e1a3b0a3d30683885
Maxwell Sayles <maxwellsayles@gmail.com>
2018-10-06 18:30:27 -0700

    not much reuse, but also ezpz.

--- day9.hs
+++ day9.hs
@@ -14,7 +14,14 @@ groups level (x:xs)
   | x == '}' = groups (level - 1) xs
   | otherwise = groups level xs
 
+garbage :: String -> [String]
+garbage "" = []
+garbage (x:xs)
+  | x == '<' = (takeWhile (/= '>') xs) : garbage (tail $ dropWhile (/= '>') xs)
+  | otherwise = garbage xs
+
 main :: IO ()
 main = do
   input <- init . sanitize <$> readFile "day9.txt"
   print $ sum $ groups 1 input
+  print $ sum $ map length $ garbage input
