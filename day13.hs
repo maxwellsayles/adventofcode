5fd56c1c9c774a6c94672bd4d61a096e3d3b0b48
Maxwell Sayles <maxwellsayles@gmail.com>
2018-10-17 07:33:51 -0700

    super simplified

--- day13.hs
+++ day13.hs
@@ -1,29 +1,20 @@
 import Control.Applicative
 
-import qualified Data.IntMap as IM
-
-parseInput :: String -> IM.IntMap Int
-parseInput = IM.fromList .
-             map (\[x, y] -> (read x, read y)) .
+parseInput :: String -> [(Int, Int)]
+parseInput = map (\[x, y] -> (read x, read y)) .
              map words .
              lines .
              filter (':' /= )
 
-cost :: IM.IntMap Int -> Int -> Int -> Int
-cost map depth time =
-  case IM.lookup depth map of
-    Just range -> let n = (range - 1) * 2
-                  in if time `mod` n == 0 then range * depth else 0
-    _ -> 0
+cost :: [(Int, Int)] -> Int
+cost = sum . map (\(d, r) -> if d `mod` (r * 2 - 2) == 0 then d * r else 0)
 
-safe :: IM.IntMap Int -> Int -> Bool
-safe input startTime =
-  all (\(depth, range) -> (depth + startTime) `mod` (range * 2 - 2) /= 0) $ IM.toList input
+safe :: Int -> [(Int, Int)] -> Bool
+safe t = all (\(d, r) -> (d + t) `mod` (r * 2 - 2) /= 0)
 
 main :: IO ()
 main = do
 --  let input = IM.fromList [(0, 3), (1, 2), (4, 4), (6, 4)]
   input <- parseInput <$> readFile "day13.txt"
-  let maxLayer = fst $ IM.findMax input
-  print $ sum $ zipWith (cost input) [0..maxLayer] [0..]
-  print $ head $ filter (safe input) [0..]
+  print $ cost input
+  print $ head $ filter (flip safe input) [0..]
