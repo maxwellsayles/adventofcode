9837b407c9f9acfb8f7371e7fb2f93dc9512364f
Maxwell Sayles <maxwellsayles@gmail.com>
2018-10-16 22:35:20 -0700

    kind of weird that my cost function did not work, but just checking all the mods worked much better

--- day13.hs
+++ day13.hs
@@ -9,13 +9,6 @@ parseInput = IM.fromList .
              lines .
              filter (':' /= )
 
-pos :: IM.IntMap Int -> Int -> Int -> Maybe Int
-pos map depth time = do
-  range <- IM.lookup depth map
-  let n = (range - 1) * 2
-      y = time `mod` n
-  return $! if y >= range then n - y else y
-
 cost :: IM.IntMap Int -> Int -> Int -> Int
 cost map depth time =
   case IM.lookup depth map of
@@ -23,8 +16,14 @@ cost map depth time =
                   in if time `mod` n == 0 then range * depth else 0
     _ -> 0
 
+safe :: IM.IntMap Int -> Int -> Bool
+safe input startTime =
+  all (\(depth, range) -> (depth + startTime) `mod` (range * 2 - 2) /= 0) $ IM.toList input
+
 main :: IO ()
 main = do
+--  let input = IM.fromList [(0, 3), (1, 2), (4, 4), (6, 4)]
   input <- parseInput <$> readFile "day13.txt"
   let maxLayer = fst $ IM.findMax input
   print $ sum $ zipWith (cost input) [0..maxLayer] [0..]
+  print $ head $ filter (safe input) [0..]
