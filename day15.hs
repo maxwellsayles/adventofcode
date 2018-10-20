7b661f5d68b4213798887e4bbdf5b119d623d7dd
Maxwell Sayles <maxwellsayles@gmail.com>
2018-10-19 20:28:37 -0700

    ezpz not intelligent

---
+++ day15.hs
@@ -0,0 +1,40 @@
+import Data.Bits
+
+cap :: Int
+cap = 2147483647
+
+factorA :: Int
+factorA = 16807
+
+factorB :: Int
+factorB = 48271
+
+initA :: Int
+--initA = 65
+initA = 289
+
+initB :: Int
+--initB = 8921
+initB = 629
+
+stepA :: Int -> Int
+stepA x = (x * factorA) `rem` cap
+
+stepB :: Int -> Int
+stepB x = (x * factorB) `rem` cap
+
+generatorA :: [Int]
+generatorA = tail $ iterate stepA initA
+
+generatorB :: [Int]
+generatorB = tail $ iterate stepB initB
+
+eq16 :: Int -> Int -> Bool
+eq16 x y =
+  let x' = x .&. 65535
+      y' = y .&. 65535
+  in x' == y'
+
+main :: IO ()
+main = do
+  print $ length $ filter (uncurry eq16) $ take 40000000 $ zip generatorA generatorB
