dbe36f9255858f00bd1de4d7a1da7ec18e08df52
Maxwell Sayles <maxwellsayles@gmail.com>
2018-11-13 21:58:40 -0800

    Implemented math for collisions, but this did not work.

--- day20.hs
+++ day20.hs
@@ -26,11 +26,13 @@ at time `t` and see if they collide.
 
 
 import Control.Applicative
-import Data.List (sortBy, sort)
+import Data.List (sortBy, sort, tails)
 import Data.Ord (comparing)
 
-data Vector3 = Vector3 Int Int Int deriving Show
-data Particle = Particle Vector3 Vector3 Vector3 deriving Show
+import qualified Data.Set as S
+
+data Vector3 = Vector3 Int Int Int deriving (Eq, Ord, Show)
+data Particle = Particle Vector3 Vector3 Vector3 deriving (Eq, Ord, Show)
 
 vector3ToHeavy :: Vector3 -> [Int]
 vector3ToHeavy (Vector3 x y z) = reverse $ sort $ map abs [x, y, z]
@@ -55,13 +57,56 @@ location (Particle (Vector3 px py pz) (Vector3 vx vy vz) (Vector3 ax ay az)) t =
       pz' = (2 * pz + (2 * vz - az) * t + az * t * t) `div` 2
   in (px', py', pz')
 
--- WIP
-intersect :: Particle -> Particle -> [Double]
-intersect (Particle (Vector3 p1 _ _) (Vector3 v1 _ _) (Vector3 a1 _ _))
-          (Particle (Vector3 p2 _ _) (Vector3 v2 _ _) (Vector3 a2 _ _)) = []
+
+collide1 :: (Double, Double, Double) -> (Double, Double, Double) -> [Double]
+collide1 (p1, v1, a1) (p2, v2, a2) =
+  let a = (a1 - a2) / 2
+      b = v1 - v2 - a
+      c = p1 - p2
+      delta = b^2 - 4 * a * c
+      s = sqrt delta
+  in  [(-b + s) / (2 * a), (-b - s) / (2 * a)]
+
+collideX :: Particle -> Particle -> [Double]
+collideX (Particle (Vector3 p1 _ _) (Vector3 v1 _ _) (Vector3 a1 _ _))
+         (Particle (Vector3 p2 _ _) (Vector3 v2 _ _) (Vector3 a2 _ _)) =
+  let p1' = fromIntegral p1
+      v1' = fromIntegral v1
+      a1' = fromIntegral a1
+      p2' = fromIntegral p2
+      v2' = fromIntegral v2
+      a2' = fromIntegral a2
+  in collide1 (p1', v1', a2') (p2', v2', a2')
+
+collideT :: Particle -> Particle -> Int -> Bool
+collideT part1 part2 t =
+  let part1' = location part1 t
+      part2' = location part2 t
+  in part1' == part2'
+
+isIntegral :: Double -> Bool
+isIntegral d = d == fromIntegral (round d)
+
+collide :: Particle -> Particle -> Bool
+collide part1 part2 =
+  any (collideT part1 part2) $
+  map round $
+  filter isIntegral $
+  filter (> 0) $
+  collideX part1 part2
+
+collisions :: [Particle] -> [Particle]
+collisions ps =
+  concatMap (\(x, y) -> [x, y]) $
+  filter (uncurry collide) $
+  [(x, y) | (x:ys) <- tails ps, y <- ys]
 
 main :: IO ()
 main = do
   input <- map parseLine . lines <$> readFile "day20.txt"
   print $ fst $ head $ sortBy (comparing snd) $ zip [0..] $
     map particleToSortedList input
+
+  let leftover = S.fromList input `S.difference` (S.fromList $ collisions input)
+  print $ S.size leftover
+  
