a7add5f0e1116b839c3d50b034008b1fa525403a
Maxwell Sayles <maxwellsayles@gmail.com>
2018-12-09 21:35:50 -0800

    Read carefully again. A 4x4 grid becomes a 6x6 grid, which is divisible by 2. I assumed that we would oscilate between 2x2 and 3x3 blocks, but that is not the case necessarily.

--- day21.hs
+++ day21.hs
@@ -1,15 +1,13 @@
 import Control.Applicative
 import Control.Arrow
 import Data.List (nub)
+import Debug.Trace (trace)
 
 import qualified Data.Map as M
 
-type Moves2 = M.Map String [String]
-type Moves3 = M.Map String [String]
-type Moves = M.Map String [String]
+type Moves = M.Map String String
+type State = String
 
--- 0 1
--- 2 3
 mapLine2 :: String -> [String]
 mapLine2 xs = nub $ map (map (xs !!)) idxs
   where idxs = [ [0, 1, 2, 3]
@@ -22,9 +20,6 @@ mapLine2 xs = nub $ map (map (xs !!)) idxs
                , [3, 1, 2, 0]
                ]
 
--- 0 1 2
--- 3 4 5
--- 6 7 8
 mapLine3 :: String -> [String]
 mapLine3 xs = nub $ map (map (xs !!)) idxs
   where idxs = [ [0, 1, 2, 3, 4, 5, 6, 7, 8]
@@ -37,20 +32,54 @@ mapLine3 xs = nub $ map (map (xs !!)) idxs
                , [8, 5, 2, 7, 4, 1, 6, 3, 0]
                ]
 
-parseInput2 :: [String] -> Moves2
+parseInput2 :: [String] -> Moves
 parseInput2 = M.fromList .
-              concatMap (\(xs, y) -> [(x, [y]) | x <- xs]) .
+              concatMap (\(xs, y) -> [(x, y) | x <- xs]) .
               map (first mapLine2 . splitAt 4) .
-              filter ((== 13). length)
+              filter ((== 13) . length)
 
-parseInput3 :: [String] -> Moves3
+parseInput3 :: [String] -> Moves
 parseInput3 = M.fromList .
-              concatMap (\(xs, ys) -> [(x, ys) | x <- xs]) .
-              map (first mapLine3 . second partition2 . splitAt 9) .
-              filter ((== 25). length)
-  where partition2 :: String -> [String]
-        partition2 xs = map (extract . flip drop xs) [0, 2, 8, 10]
-          where extract xs = map (xs !!) [0, 1, 4, 5]
+              concatMap (\(xs, y) -> [(x, y) | x <- xs]) .
+              map (first mapLine3 . splitAt 9) .
+              filter ((== 25) . length)
+
+isqrt :: Int -> Int
+isqrt = ceiling . sqrt . fromIntegral
+
+chunk :: Int -> [a] -> [[a]]
+chunk n xs = helper xs
+  where helper [] = []
+        helper xs = take n xs : helper (drop n xs)
+
+everyOther :: [a] -> [a]
+everyOther [] = []
+everyOther [x] = [x]
+everyOther (x:_:xs) = x : everyOther xs
+
+partition :: Int -> [a] -> [[a]]
+partition n xs =
+  let d = isqrt $ length xs
+      rows = chunk d xs
+      rowBlocks = chunk n (map (chunk n) rows)
+  in concatMap collect rowBlocks
+  where
+    collect block
+      | all null block = []
+      | otherwise =
+          concatMap head block : collect (map tail block)
+
+melt :: [[a]] -> [a]
+melt xs =
+  let n = isqrt $ length $ head xs
+      d = isqrt $ length xs
+      blocks = map (chunk n) xs
+  in concatMap collect $ chunk d blocks
+  where
+    collect row
+      | all null row = []
+      | otherwise =
+          concatMap head row ++ collect (map tail row)
 
 parseInput :: [String] -> Moves
 parseInput xs = (parseInput2 xs) `M.union` (parseInput3 xs)
@@ -58,19 +87,23 @@ parseInput xs = (parseInput2 xs) `M.union` (parseInput3 xs)
 testInput :: String
 testInput = "../.# => ##./#../...\n.#./..#/### => #..#/..../..../#..#"
 
-initState :: [String]
-initState = [".#...####"]
+initState :: State
+initState = ".#...####"
 
-step :: Moves -> [String] -> [String]
-step ms = concatMap (ms M.!)
+step :: Moves -> State -> State
+step moves state =
+  let d = if length state `mod` 2 == 0 then 2 else 3
+      blocks = partition d state
+  in trace (show blocks) $
+     melt $ map (moves M.!) blocks
 
-countOn :: [String] -> Int
-countOn = length . filter (== '#') . concat
+countOn :: State -> Int
+countOn = length . filter (== '#')
 
 main :: IO ()
 main = do
 --  let input = map (filter (\c -> c `elem` ".#")) . lines $ testInput
   input <- map (filter (\c -> c `elem` ".#")) . lines <$> readFile "day21.txt"
   let moves = parseInput input
-  print (moves M.! "#######.#")
-  mapM_ (print . countOn) $ (take 7) $ iterate (step moves) initState
+  print $ countOn $ (!! 5) $ iterate (step moves) initState
+
