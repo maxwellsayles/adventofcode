ca7085cc3935b3fd32f722e40264f3efbe47d505
Maxwell Sayles <maxwellsayles@gmail.com>
2019-01-06 21:36:40 -0800

    bruteforce, but it is too slow

--- 2018/day01.fs
+++ 2018/day01.fs
@@ -1,5 +1,21 @@
+module Seq = let rec cycle xs = seq { yield! xs; yield! cycle xs }
+
+let rec firstRepeat (xs: seq<int>) (acc: int) (visited: Set<int>) =
+    printfn "%d" acc
+    if visited.Contains(acc) then acc
+    else firstRepeat (Seq.tail xs) (acc + Seq.head xs) (visited.Add acc)
+
 [<EntryPoint>]
 let main args =
     let input = Seq.map int <| System.IO.File.ReadLines("day01.txt")
     printfn "%d" <| Seq.sum input
+
+    let input =
+        System.IO.File.ReadLines("day01.txt")
+        |> Seq.map int
+        |> Seq.toList
+        |> Seq.cycle
+
+    printfn "%d" <| firstRepeat input 0 Set.empty
+
     0
