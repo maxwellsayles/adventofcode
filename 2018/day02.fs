10fc4fc5608e86639d10f6c51d7b148223159a70
Maxwell Sayles <maxwellsayles@gmail.com>
2019-01-27 14:44:27 -0800

    still ezpz

--- 2018/day02.fs
+++ 2018/day02.fs
@@ -4,10 +4,23 @@ let count str =
     let three = Seq.exists (fun (_, x) -> x = 3) hist
     ((if two then 1 else 0), (if three then 1 else 0))
 
+let common xs ys =
+    Seq.zip xs ys
+    |> Seq.filter (fun (x, y) -> x = y)
+    |> Seq.map fst
+    |> Array.ofSeq |> (fun x -> new string(x))
+
 [<EntryPoint>]
 let main args =
-    let input = System.IO.File.ReadLines("day02.txt")
+    let input = System.IO.File.ReadLines("day02.txt") |> List.ofSeq
     let (twos, threes) = Seq.map count input |> List.ofSeq |> List.unzip
     let checksum = Seq.sum twos * Seq.sum threes
     printfn "%d" checksum
+
+    let n = Seq.head input |> String.length
+    seq { for x in input do for y in input do yield common x y }
+    |> Seq.filter (fun (x: string) -> x.Length = n - 1)
+    |> Seq.head
+    |> printfn "%s"
+
     0
