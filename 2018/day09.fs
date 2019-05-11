a7210adf364374d7c5af42426f137e9f2cc04aea
Maxwell Sayles <maxwellsayles@gmail.com>
2019-05-11 16:01:41 -0700

    not too bad once you have the right data structure. not super fast though

---
+++ 2018/day09.fs
@@ -0,0 +1,42 @@
+(**
+ * Build with
+ * $ fsharpc -a Ring.fs
+ * $ fsharpc --reference:Ring.dll day09.fs
+ *
+ * or as a single command line:
+ * $ fsharpc -a Ring.fs && fsharpc --reference:Ring.dll day09.fs && ./day09.exe
+ *)
+
+open Ring
+
+let numPlayers = 464
+let numMarbles = 70918
+
+let step x ring =
+    if (x % 23) <> 0
+    then
+        Ring.spinCCW 2 ring
+        |> Ring.push x
+        |> fun ring -> 0, ring
+    else 
+        let ring' = Ring.spinCW 7 ring
+        x + Ring.top ring', Ring.pop ring'
+
+let rec gameStep x ring (scores: Map<int, int>) =
+    if x = numMarbles + 1
+    then scores // Game over!
+    else 
+        let score, ring' = step x ring
+        let player = (x % numPlayers) + 1
+        let scores' = Map.add player (scores.[player] + score) scores
+        gameStep (x + 1) ring' scores'
+
+[<EntryPoint>]
+let main args =
+    let initScores =
+        List.init numPlayers (fun i -> i + 1, 0)
+        |> Map.ofList
+    let finalScores = gameStep 1 (Ring.singleton 0) initScores
+    let winningScore = finalScores |> Map.toList |> List.map snd |> List.max
+    printfn "%d" winningScore
+    0
