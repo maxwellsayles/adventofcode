2e229d1c2146fe9a28ee150c628547968310a5b0
Maxwell Sayles <maxwellsayles@gmail.com>
2019-05-11 18:26:12 -0700

    Turns out that the heuristic used to determine when to balance makes a huge difference. The performance is 100x if we only balance when one of the internal lists is empty.

--- 2018/Ring.fs
+++ 2018/Ring.fs
@@ -7,15 +7,34 @@ type Ring = {
 
 let toList ring = List.append ring.xs (List.rev ring.ys)
 
+// Only balance the internal lists if one of them is empty.
 let private balance ring =
-    let n = List.length ring.xs
-    let m = List.length ring.ys
-    if n >= 2 * m || m >= 2 * n then
-        let vs = toList ring
-        let p = (n + m) / 2
-        { xs = List.take p vs;
-          ys = List.skip p vs |> List.rev }
+    if List.isEmpty ring.xs then
+        let n = List.length ring.ys / 2
+        {
+            xs = List.skip n ring.ys |> List.rev;
+            ys = List.take n ring.ys;
+        }
+     elif List.isEmpty ring.ys then
+        let n = List.length ring.xs / 2
+        {
+            xs = List.take n ring.xs;
+            ys = List.skip n ring.xs |> List.rev;
+        }
     else ring
+
+// This was an alternative strategy for balancing the ring lists to achieve
+// amortized O(1) operations, but this is so much slower than the above, 
+// because the balancing occurs more frequently.
+// let private balance ring =    
+//     let n = List.length ring.xs
+//     let m = List.length ring.ys
+//     if n >= 2 * m || m >= 2 * n then
+//         let vs = toList ring
+//         let p = (n + m) / 2
+//         { xs = List.take p vs;
+//           ys = List.skip p vs |> List.rev }
+//     else ring
 
 let ofList xs = balance { xs = xs; ys = List.empty }
 
--- 2018/day09.fs
+++ 2018/day09.fs
@@ -10,7 +10,8 @@
 open Ring
 
 let numPlayers = 464
-let numMarbles = 70918
+//let numMarbles = 70918 // Part 1
+let numMarbles = 7091800
 
 let step x ring =
     if (x % 23) <> 0
@@ -22,19 +23,19 @@ let step x ring =
         let ring' = Ring.spinCW 7 ring
         x + Ring.top ring', Ring.pop ring'
 
-let rec gameStep x ring (scores: Map<int, int>) =
+let rec gameStep x ring (scores: Map<int, uint64>) =
     if x = numMarbles + 1
     then scores // Game over!
     else 
         let score, ring' = step x ring
         let player = (x % numPlayers) + 1
-        let scores' = Map.add player (scores.[player] + score) scores
+        let scores' = Map.add player (scores.[player] + uint64 score) scores
         gameStep (x + 1) ring' scores'
 
 [<EntryPoint>]
 let main args =
     let initScores =
-        List.init numPlayers (fun i -> i + 1, 0)
+        List.init numPlayers (fun i -> i + 1, 0UL)
         |> Map.ofList
     let finalScores = gameStep 1 (Ring.singleton 0) initScores
     let winningScore = finalScores |> Map.toList |> List.map snd |> List.max
