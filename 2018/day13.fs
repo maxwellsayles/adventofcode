c0b06730d673ae9895b8d08f8430bc075e25920e
Maxwell Sayles <maxwellsayles@gmail.com>
2019-08-11 22:08:47 -0700

    reformat input into Array2D

--- 2018/day13.fs
+++ 2018/day13.fs
@@ -1,8 +1,11 @@
 
+let input: char[,] =
+    let inputRaw = System.IO.File.ReadAllLines("day13.txt")
+    let height = Array.length inputRaw
+    let width = String.length inputRaw.[0]
+    Array2D.init width height (fun x y -> inputRaw.[y].[x])
+
 [<EntryPoint>]
 let main args =
-    let input =
-        System.IO.File.ReadAllLines("day13.txt")
-        |> Array.map Array.ofSeq
-    printfn "%d %d" (Array.length input) (Array.length input.[0])
+    printfn "%d %d" (Array2D.length1 input) (Array2D.length2 input)
     0
