377db81e6eafc2081cc58ec3edfedda424c969b2
Maxwell Sayles <maxwellsayles@gmail.com>
2019-08-12 21:59:13 -0700

    parse the initial input into a blank map and initial cart state.

--- 2018/day13.fs
+++ 2018/day13.fs
@@ -1,11 +1,43 @@
 
-let input: char[,] =
+type Map = char[,]
+
+type Turn = LeftTurn | StraightTurn | RightTurn
+
+type CartState(x: int, y: int, dx: int, dy: int, turn: Turn) =
+    new(x: int, y: int, dx: int, dy: int) = CartState(x, y, dx, dy, LeftTurn)
+    override self.ToString() = sprintf "%d %d %d %d" x y dx dy
+    member this.Step(map: Map) = new CartState(x, y, dx, dy, turn) // TODO
+
+let input: Map =
     let inputRaw = System.IO.File.ReadAllLines("day13.txt")
     let height = Array.length inputRaw
     let width = String.length inputRaw.[0]
     Array2D.init width height (fun x y -> inputRaw.[y].[x])
 
+let width = Array2D.length1 input
+let height = Array2D.length2 input
+
+let map: Map =
+    Array2D.map (function
+                 | '>' | '<' -> '-'
+                 | '^' | 'v' -> '|'
+                 | c -> c
+                 ) input
+
+let initCartStates: list<CartState> =
+    let mutable states = List.empty
+    Array2D.iteri (fun x y -> function
+                   | '>' -> states <- new CartState(x, y, 1, 0) :: states
+                   | '<' -> states <- new CartState(x, y, -1, 0) :: states
+                   | '^' -> states <- new CartState(x, y, 0, -1) :: states
+                   | 'v' -> states <- new CartState(x, y, 0, 1) :: states
+                   | _ -> ()
+                   ) input
+    states
+    
 [<EntryPoint>]
 let main args =
-    printfn "%d %d" (Array2D.length1 input) (Array2D.length2 input)
+    printfn "%d %d" width height
+    printfn "%A" initCartStates
+    printfn "%d" (List.length initCartStates)
     0
