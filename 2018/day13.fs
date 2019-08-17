d377e20d1f6ef70b22408ae4697c52c207f26bbb
Maxwell Sayles <maxwellsayles@gmail.com>
2019-08-16 23:02:36 -0700

    honestly, should just be using list.sortby instead of the really complicated set comparison stuff

--- 2018/day13.fs
+++ 2018/day13.fs
@@ -4,7 +4,6 @@ type Map = char[,]
 
 type Turn = LeftTurn | StraightTurn | RightTurn
 
-[<CustomComparison; CustomEquality; Struct>]
 type CartState(x: int, y: int, dx: int, dy: int, turn: Turn) =
     member this.X = x
     member this.Y = y
@@ -12,35 +11,7 @@ type CartState(x: int, y: int, dx: int, dy: int, turn: Turn) =
     new(x: int, y: int, dx: int, dy: int) = CartState(x, y, dx, dy, LeftTurn)
     override self.ToString() = sprintf "%d %d %d %d" x y dx dy
 
-    member private this.Compare (that: CartState) = 
-        if this.Y < that.Y then -1
-            elif this.Y > that.Y then 1
-            elif this.X < that.X then -1
-            elif this.X > that.X then 1
-            else 0
-
-    interface IComparable<CartState> with
-        member this.CompareTo that = this.Compare that
-
-    interface IComparable with
-        member this.CompareTo that =
-            match that with
-              | :? CartState as that' -> this.Compare that'
-              | _ -> invalidArg "obj" "not a Category"
-
-    override this.Equals(that) =
-        match that with
-            | :? CartState as that' -> this.X = that'.X && this.Y = that'.Y
-            | _ -> false
-
-    interface IEquatable<CartState> with
-        member this.Equals(that: CartState) =
-            this.Equals(that)
-
-    override this.GetHashCode() = (this.X, this.Y).GetHashCode()
-
     member this.Step(map: Map) = new CartState(x, y, dx, dy, turn) // TODO
-
 
 let input: Map =
     let inputRaw = System.IO.File.ReadAllLines("day13.txt")
@@ -58,20 +29,22 @@ let map: Map =
                  | c -> c
                  ) input
 
-let initCartStates: Set<CartState> =
-    let mutable states = Set.empty
+let initCartStates: list<CartState> =
+    let mutable states = List.empty
     Array2D.iteri (fun x y -> function
-                   | '>' -> states <- Set.add (new CartState(x, y, 1, 0)) states
-                   | '<' -> states <- Set.add (new CartState(x, y, -1, 0)) states
-                   | '^' -> states <- Set.add (new CartState(x, y, 0, -1)) states 
-                   | 'v' -> states <- Set.add (new CartState(x, y, 0, 1)) states
+                   | '>' -> states <- new CartState(x, y, 1, 0) :: states
+                   | '<' -> states <- new CartState(x, y, -1, 0) :: states
+                   | '^' -> states <- new CartState(x, y, 0, -1) :: states 
+                   | 'v' -> states <- new CartState(x, y, 0, 1) :: states
                    | _ -> ()
                    ) input
     states
+
+let orderedStates (states: list<CartState>): list<CartState> =
+    List.sortBy (fun (s: CartState) -> s.X, s.Y) states
     
 [<EntryPoint>]
 let main args =
     printfn "%d %d" width height
-    printfn "%A" initCartStates
-    printfn "%d" (Set.count initCartStates)
+    printfn "%A" (orderedStates initCartStates)
     0
