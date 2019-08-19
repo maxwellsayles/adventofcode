53989ef73fdfdc9e1e147f171c19106468787488
Maxwell Sayles <maxwellsayles@gmail.com>
2019-08-18 22:47:20 -0700

    use a vector and initial board

--- 2018/day14.fs
+++ 2018/day14.fs
@@ -1,1 +1,13 @@
+// fsharpc -r:FSharpx.Collections.dll day14.fs
+
+open FSharpx.Collections
+
+module PV = PersistentVector
+
 let input = 556061
+
+[<EntryPoint>]
+let main args =
+    let vec: PersistentVector<int> = PV.ofSeq [3;7]
+    printfn "%A" vec
+    0
