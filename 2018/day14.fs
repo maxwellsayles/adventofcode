f267b9b8f1f7482d845d741f389eaa1dbc845e50
Maxwell Sayles <maxwellsayles@gmail.com>
2019-08-19 22:04:20 -0700

    ezpz

--- 2018/day14.fs
+++ 2018/day14.fs
@@ -3,11 +3,38 @@
 open FSharpx.Collections
 
 module V = PersistentVector
+type State = PersistentVector<int>
 
 let input = 556061
+let initState: State = V.ofSeq [3;7]
+
+let step (i: int) (j: int) (s: State) : int * int * State =
+    let x = V.nth i s
+    let y = V.nth j s
+    let v = x + y
+    let s' = if v >= 10
+             then V.conj 1 s |> V.conj (v - 10)
+             else V.conj v s
+    let n = V.length s'
+    let i' = (i + x + 1) % n
+    let j' = (j + y + 1) % n
+    i', j', s'
+
+let iterate (n: int) : State =
+    let rec helper (n: int) (i: int) (j: int) (s: State) : State =
+        if n = 0
+        then s
+        else let i', j', s' = step i j s
+             helper (n - 1) i' j' s'
+    helper n 0 1 initState
 
 [<EntryPoint>]
 let main args =
-    let vec: PersistentVector<int> = V.ofSeq [3;7]
-    printfn "%A" vec
+    let finalState = iterate (input + 10)
+    let solution =
+        List.map (fun i -> V.nth (i + input) finalState) [0..9]
+        |> List.map (sprintf "%d")
+        |> List.toArray
+        |> System.String.Concat
+    printfn "%s" solution
     0
