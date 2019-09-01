03c373a6180becb58bd5b550725dcdd5555c3c7e
Maxwell Sayles <maxwellsayles@gmail.com>
2019-08-31 22:30:48 -0700

    Updated the types to handle all players and their "team"

--- 2018/day15.fs
+++ 2018/day15.fs
@@ -1,46 +1,56 @@
-// fsharpc -r:FSharpx.Collections.dll day14.fs
+// fsharpc -r:FSharpx.Collections.dll day15.fs
 
 open FSharpx.Collections
 
 module Q = Queue
 
+type Team = Elves | Goblins
+
 // The record order is important so the set is ordered in reading order
 type Point = { y: int; x: int }
+type Player = { y: int; x: int; team: Team } with
+    member this.Point = { x = this.x; y = this.y }
 
 type Grid = string []
 type Path = list<Point>
-type Team = Set<Point>
 
-let (grid: Grid, initElves: Team, initGoblins: Team) =
+let (grid: Grid, initPlayers: Set<Player>) =
     let inputRaw = System.IO.File.ReadAllLines("day15.txt")
-    let g = Array.map (String.map (fun c -> if c = '#' then '#' else '.')) inputRaw
+    let grid =
+        Array.map (String.map (fun c -> if c = '#' then '#' else '.')) inputRaw
     let height = inputRaw.Length
     let width = inputRaw.[0].Length
-    let helper f =
+    let helper c t =
         [for x in [0 .. width - 1] do
          for y in [0 .. height - 1] do
-         if f inputRaw.[y].[x] then yield {x = x; y = y}]
+         if inputRaw.[y].[x] = c then yield {x = x; y = y; team = t}]
         |> Set.ofList
-    let es = helper (fun c -> c = 'E')
-    let gs = helper (fun c -> c = 'G')
-    g, es, gs
+    let players = Set.union (helper 'E' Elves) (helper 'G' Goblins)
+    grid, players
 
-let isAdjacent (p: Point) (ps: Team) =
-    Set.contains { x = p.x - 1; y = p.y } ps ||
-    Set.contains { x = p.x + 1; y = p.y } ps ||
-    Set.contains { x = p.x; y = p.y - 1 } ps ||
-    Set.contains { x = p.x; y = p.y + 1 } ps
+let otherTeam = function Elves -> Goblins | Goblins -> Elves
 
-let isValidMove (visited: Set<Point>) (p: Point) =
-    not (Set.contains p visited) && grid.[p.y].[p.x] = '.'
+let isAdjacent (p: Player) (ps: Set<Player>) =
+    let t = otherTeam p.team
+    Set.contains { x = p.x - 1; y = p.y; team = t } ps ||
+    Set.contains { x = p.x + 1; y = p.y; team = t } ps ||
+    Set.contains { x = p.x; y = p.y - 1; team = t } ps ||
+    Set.contains { x = p.x; y = p.y + 1; team = t } ps
 
-let shortestPath (p: Point) (ps: Team): list<Point> =
+let isValidMove (players: Set<Player>) (visited: Set<Point>) (p: Point) =
+    grid.[p.y].[p.x] = '.' &&
+    not (Set.contains { x = p.x; y = p.y; team = Elves } players ||
+         Set.contains { x = p.x; y = p.y; team = Goblins } players) &&
+    not (Set.contains p visited)
+
+let shortestPath (player: Player) (players: Set<Player>): list<Point> =
+    let team = player.team
     let rec helper (q: Queue<Path>) (visited: Set<Point>) =
         if Queue.isEmpty q
         then failwith "No path."
         else let path = Queue.head q
              let p = List.head path
-             if isAdjacent p ps
+             if isAdjacent { x = p.x; y = p.y; team = team } players
              then path
              else let ps =
                       [ { x = p.x - 1; y = p.y }
@@ -48,16 +58,16 @@ let shortestPath (p: Point) (ps: Team): list<Point> =
                         { x = p.x; y = p.y - 1 }
                         { x = p.x; y = p.y + 1 }
                         ]
-                      |> List.filter (isValidMove visited)
+                      |> List.filter (isValidMove players visited)
                   let q' =
                       List.fold (fun acc p -> Queue.conj (p :: path) acc) q ps
                   let visited' = Set.union visited (Set.ofList ps)
                   helper q' visited'
-    helper (Queue.ofList [[p]]) Set.empty
+    let q = Queue.ofList [[player.Point]]
+    helper q Set.empty
 
 [<EntryPoint>]
 let main args =
     printfn "%A" grid
-    printfn "%A" initElves
-    printfn "%A" initGoblins
+    printfn "%A" initPlayers
     0
