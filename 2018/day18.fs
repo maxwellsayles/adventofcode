23228bd8d8e5d8b1791b4be66955db36c731b284
Maxwell Sayles <maxwellsayles@gmail.com>
2019-09-20 22:26:05 -0700

    just for fun, the constant memory period finding

--- 2018/day18.fs
+++ 2018/day18.fs
@@ -39,17 +39,17 @@ let countGrid (c: char) (grid: char [,]) : int =
     Array2D.iter (fun v -> if v = c then count <- count + 1 else ()) grid
     count
 
-let rec findCycle (i: int) (grid: char [,]) (acc: Map<char [,], int>) : int * int =
-    if Map.containsKey grid acc
-    then i, Map.find grid acc
-    else findCycle (i + 1) (stepGrid grid) (Map.add grid i acc)
+let rec findCycle (i: int) (grid1: char [,]) (j: int) (grid2: char [,]) : int * int =
+    if grid1 = grid2
+    then i, j
+    else findCycle (i + 1) (stepGrid grid1) (j + 2) (stepGrid (stepGrid grid2))
 
 [<EntryPoint>]
 let main (args : string []) : int = 
     let grid10 = iterate 10 input
     printfn "%d" (countGrid '|' grid10 * countGrid '#' grid10)
 
-    let x1, x0 = (findCycle 0 input Map.empty)
+    let x1, x0 = findCycle 0 input 2 (stepGrid (stepGrid input))
     let i = (1000000000 - x0) % (x1 - x0) + x0
     printfn "Finding iteration: %d" i
     let gridBig = iterate i input
