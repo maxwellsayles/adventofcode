a03759accf07961529a1a3dabeb1c191001a9c30
Maxwell Sayles <maxwellsayles@gmail.com>
2019-10-04 23:38:26 -0700

    mutable array instead of a vector. maybe it is faster

--- 2018/day21.fs
+++ 2018/day21.fs
@@ -3,36 +3,12 @@
 open FSharpx.Collections
 open System
 
-module V = PersistentVector
-type V<'a> = PersistentVector<'a>
-
-type Regs = V<int>
-
-type Instr = Instr of String * int * int * int
-
-let exec (Instr (op, a, b, c)) (rs: Regs) : Regs =
-    match op with
-    | "addr" -> V.update c (V.nth a rs + V.nth b rs) rs
-    | "addi" -> V.update c (V.nth a rs + b) rs
-    | "mulr" -> V.update c (V.nth a rs * V.nth b rs) rs
-    | "muli" -> V.update c (V.nth a rs * b) rs
-    | "banr" -> V.update c (V.nth a rs &&& V.nth b rs) rs
-    | "bani" -> V.update c (V.nth a rs &&& b) rs
-    | "borr" -> V.update c (V.nth a rs ||| V.nth b rs) rs
-    | "bori" -> V.update c (V.nth a rs ||| b) rs
-    | "setr" -> V.update c (V.nth a rs) rs
-    | "seti" -> V.update c a rs
-    | "gtir" -> V.update c (if a > V.nth b rs then 1 else 0) rs
-    | "gtri" -> V.update c (if V.nth a rs > b then 1 else 0) rs
-    | "gtrr" -> V.update c (if V.nth a rs > V.nth b rs then 1 else 0) rs
-    | "eqir" -> V.update c (if a = V.nth b rs then 1 else 0) rs
-    | "eqri" -> V.update c (if V.nth a rs = b then 1 else 0) rs
-    | "eqrr" -> V.update c (if V.nth a rs = V.nth b rs then 1 else 0) rs
-    | s -> failwith (sprintf "WTF: %s" s)
+type Regs = int []
+type Instr = String * int * int * int
 
 let parseInstruction (s: String) : Instr =
     let xs = s.Split [|' '|]
-    Instr (xs.[0], int xs.[1], int xs.[2], int xs.[3])
+    (xs.[0], int xs.[1], int xs.[2], int xs.[3])
 
 let ipReg, instrs : int * Instr [] =
     let lines =
@@ -48,42 +24,61 @@ let ipReg, instrs : int * Instr [] =
         |> Array.ofSeq
     ipReg, instrs
 
+let exec (rs: Regs) : unit =
+    let ip = rs.[ipReg]
+    let (op, a, b, c) = instrs.[ip]
+    match op with
+    | "addr" -> rs.[c] <- rs.[a] + rs.[b]
+    | "addi" -> rs.[c] <- rs.[a] + b
+    | "mulr" -> rs.[c] <- rs.[a] * rs.[b]
+    | "muli" -> rs.[c] <- rs.[a] * b
+    | "banr" -> rs.[c] <- rs.[a] &&& rs.[b]
+    | "bani" -> rs.[c] <- rs.[a] &&& b
+    | "borr" -> rs.[c] <- rs.[a] ||| rs.[b]
+    | "bori" -> rs.[c] <- rs.[a] ||| b
+    | "setr" -> rs.[c] <- rs.[a]
+    | "seti" -> rs.[c] <- a
+    | "gtrr" -> rs.[c] <- if rs.[a] > rs.[b] then 1 else 0
+    | "gtir" -> rs.[c] <- if a > rs.[b] then 1 else 0
+    | "gtri" -> rs.[c] <- if rs.[a] > b then 1 else 0
+    | "eqrr" -> rs.[c] <- if rs.[a] = rs.[b] then 1 else 0
+    | "eqir" -> rs.[c] <- if a = rs.[b] then 1 else 0
+    | "eqri" -> rs.[c] <- if rs.[a] = b then 1 else 0
+    | s -> failwith (sprintf "WTF: %s" s)
+    let ip' = rs.[ipReg]
+    rs.[ipReg] <- ip' + 1
+
 let rec getArg (rs: Regs) : int =
-    let ip = V.nth ipReg rs
-    if ip = 28
-    then V.nth 3 rs
+    let ip = rs.[ipReg]
+    if ip = 28 then
+        rs.[3]
     else
-        let instr = instrs.[ip]
-        let rs' = exec instr rs
-        let ip' = V.nth ipReg rs'
-        getArg <| V.update ipReg (ip' + 1) rs'
+        exec rs
+        getArg rs
 
 let mutable cnt = 0
 let rec getRep (rs: Regs) (acc: Set<int>) : int =
-    let ip = V.nth ipReg rs
-    let rs' =
-        let instr = instrs.[ip]
-        let rs' = exec instr rs
-        let ip' = V.nth ipReg rs'
-        V.update ipReg (ip' + 1) rs'
+    let ip = rs.[ipReg]
     if ip = 28 then
-        let r3 = V.nth 3 rs
+        let r3 = rs.[3]
         printfn "%d" cnt
         cnt <- cnt + 1
         if Set.contains r3 acc then
             r3
         else
-            getRep rs' (Set.add r3 acc)
+            exec rs
+            getRep rs (Set.add r3 acc)
     else
-        getRep rs' acc
+        exec rs
+        getRep rs acc
 
 [<EntryPoint>]
 let main args = 
     printfn "#ip: %d" ipReg
-    let r3 = getArg (V.ofSeq [0; 0; 0; 0; 0; 0])
+    let r3 = getArg [|0; 0; 0; 0; 0; 0|]
     printfn "ip=28, r3=%d" r3
 
-    let r3' = getRep (V.ofSeq [0; 0; 0; 0; 0; 0]) Set.empty
-    printfn "ip=28, r3=%d" r3'
+    // let r3' = getRep (V.ofSeq [0; 0; 0; 0; 0; 0]) Set.empty
+    // printfn "ip=28, r3=%d" r3'
         
     0
