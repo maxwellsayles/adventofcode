3efc05e3892518da02053ab8d0fae2a90e433c3d
Maxwell Sayles <maxwellsayles@gmail.com>
2019-10-24 23:19:42 -0700

    well that was unreasonably difficult. maybe easier with glpk or writing my own simplex, but good chance to learn about Z3. but why this problem needed linear programming is a bit much

--- 2018/day23.fs
+++ 2018/day23.fs
@@ -35,19 +35,20 @@ let part1 : int =
     |> List.length
 
 let ctx = new Context()
-let intExpr (i: int) : ArithExpr = ctx.MkIntConst(string i) :> ArithExpr
+let intExpr (i: int) : ArithExpr = ctx.MkInt(string i) :> ArithExpr
 let zeroExpr : ArithExpr = intExpr 0
 let oneExpr : ArithExpr = intExpr 1
 
 let absExpr (x: ArithExpr) : ArithExpr =
-    ctx.MkITE(ctx.MkLt(x, zeroExpr), ctx.MkSub(zeroExpr, x), x)
+    ctx.MkITE(ctx.MkGt(x, zeroExpr), x, ctx.MkSub(zeroExpr, x))
     :?> ArithExpr
 
-let distanceExpr (x1: ArithExpr) (x2: ArithExpr) : ArithExpr =
+let distanceExpr (x2: ArithExpr) (x1: ArithExpr) : ArithExpr =
     ctx.MkSub(x2, x1)
     |> absExpr
 
-let distance3Expr ((x1, y1, z1): ArithExpr * ArithExpr * ArithExpr) ((x2, y2, z2): ArithExpr * ArithExpr * ArithExpr) : ArithExpr =
+let distance3Expr ((x2, y2, z2): ArithExpr * ArithExpr * ArithExpr)
+                  ((x1, y1, z1): ArithExpr * ArithExpr * ArithExpr) : ArithExpr =
     ctx.MkAdd(distanceExpr x2 x1, distanceExpr y2 y1, distanceExpr z2 z1)
 
 let inRangeExpr (p1: ArithExpr * ArithExpr * ArithExpr) (p2: ArithExpr * ArithExpr * ArithExpr) (r: ArithExpr) : ArithExpr =
@@ -67,38 +68,18 @@ let main args =
     let p = x, y, z
 
     let inRangeExprs =
-        List.map (fun (bot: NanobotState) -> inRangeExpr p (botExpr bot) (intExpr bot.r)) input
-    let sum = List.fold (fun acc expr -> ctx.MkAdd(acc, expr)) zeroExpr inRangeExprs
+        Array.ofSeq input
+        |> Array.map (fun (bot: NanobotState) -> inRangeExpr p (botExpr bot) (intExpr bot.r))
+    let sum = ctx.MkAdd(inRangeExprs)
     let minDist = distance3Expr p (zeroExpr, zeroExpr, zeroExpr)
     let opt = ctx.MkOptimize()
-    opt.MkMaximize(sum) |> ignore
-    opt.MkMinimize(minDist) |> ignore
-    opt.Check() |> ignore
-    printfn "%A" (opt.Model.ConstInterp(x))
-    printfn "%A" (opt.Model.ConstInterp(y))
-    printfn "%A" (opt.Model.ConstInterp(z))
-
-
-    // let ctx = new Context()
-    // let x = ctx.MkIntConst("x")
-    // let y = ctx.MkIntConst("y")
-    // let z = ctx.MkIntConst("z")
-    // let c1 = ctx.MkGe(x, ctx.MkInt("1"))
-    // let c2 = ctx.MkEq(z, ctx.MkInt("5"))
-    // let c3 = ctx.MkLt(x, y)
-    // let c4 = ctx.MkLt(y, z)
-    // let opt = ctx.MkOptimize()
-
-    // opt.Assert(c1)
-    // opt.Assert(c2)
-    // opt.Assert(c3)
-    // opt.Assert(c4)
-    // opt.MkMinimize(x) |> ignore
-    // opt.MkMaximize(y) |> ignore
-    // opt.Check() |> ignore
-    // printfn "%A" (opt.Model.ConstInterp(x))
-    // printfn "%A" (opt.Model.ConstInterp(y))
-    // printfn "%A" (opt.Model.ConstInterp(z))
+    let sumHandle = opt.MkMaximize(sum)
+    let minDistHandle = opt.MkMinimize(minDist)
+    printfn "%A" (opt.Check())
+    let xint = sprintf "%A" (opt.Model.ConstInterp(x)) |> int
+    let yint = sprintf "%A" (opt.Model.ConstInterp(y)) |> int
+    let zint = sprintf "%A" (opt.Model.ConstInterp(z)) |> int
+    printfn "%d" (abs xint + abs yint + abs zint)
 
     0
 
