c92b668edf8c5990757bb6581b81cc41a915b440
Maxwell Sayles <maxwellsayles@gmail.com>
2019-11-08 22:56:55 -0800

    parsing implemented. now to read what the puzzle is all about :P

--- 2018/day24.fs
+++ 2018/day24.fs
@@ -1,3 +1,10 @@
+open System.Text.RegularExpressions
+
+let (|Regex|_|) pattern input =
+        let m = Regex.Match(input, pattern)
+        if m.Success then Some(List.tail [ for g in m.Groups -> g.Value ])
+        else None
+
 type AttackType =
     | Bludgeoning
     | Cold
@@ -11,7 +18,7 @@ let stringToAttack : string -> AttackType = function
     | "fire" -> Fire
     | "radiation" -> Radiation
     | "slashing" -> Slashing
-    | unknown -> failwith <| sprintf "Unrecognized attack: %s" unknown
+    | unknown -> failwith <| sprintf "Unrecognized attack: \"%s\"" unknown
 
 type State = {
     attackPoints: int
@@ -23,24 +30,60 @@ type State = {
     weaknesses: list<AttackType>
 }
 
-let parseLine = 0
+let parseWeaknessesAndImmunities (s: string) : list<AttackType> * list<AttackType> =
+    let step (ws, is) s =
+        match s with
+        | Regex @"(immune to |weak to )(.*)" [thing; ls] ->
+            let attacks =
+                ls.Split [|','|]
+                |> Array.map (fun s -> s.Trim(' ', ')'))
+                |> Array.map stringToAttack
+                |> List.ofSeq
+            if thing = "immune to " then
+                (ws, List.append is attacks)
+            else
+                (List.append ws attacks, is)
+        | _ -> failwith <| sprintf "Could not parse: %s" s
+            
+    s.Split [|';'|]
+    |> Array.fold step (List.empty, List.empty)
 
-let rawImmuneSystem, rawInfection =
+let parseLine (s: string) : State =
+    match s with
+    | Regex @"(\d+) units each with (\d+) hit points (\(.*\) )?with an attack that does (\d+) (\w+) damage at initiative (\d+)" [units; hitPoints; extra; attackPoints; attackType; initiative] ->
+        let weaknesses, immunities =
+            if extra = "" then
+                List.empty, List.empty
+            else
+                parseWeaknessesAndImmunities extra
+        { attackPoints = int attackPoints;
+          attackType = stringToAttack attackType;
+          hitPoints = int hitPoints;
+          immunities = immunities;
+          initiative = int initiative;
+          units = int units;
+          weaknesses = weaknesses;
+        }
+    | _ -> failwith <| sprintf "Could not parse: %s" s
+
+let initImmuneSystem, initInfection =
     let lines = System.IO.File.ReadAllLines("day24.txt")
     let immune =
         Seq.takeWhile (fun (s: string) -> s.Length <> 0) lines
         |> List.ofSeq
         |> List.tail
+        |> List.map parseLine
     let infect =
         Seq.skip (Seq.length immune + 2) lines
         |> List.ofSeq
         |> List.tail
+        |> List.map parseLine
     immune, infect
 
 [<EntryPoint>]
 let main args =
-    printfn "%A" rawImmuneSystem
+    printfn "%A" initImmuneSystem
     printfn ""
-    printfn "%A" rawInfection
+    printfn "%A" initInfection
 
     0
