b736ae18fb789b0a5e9b39635a804b3019dd1d91
Maxwell Sayles <maxwellsayles@gmail.com>
2019-11-27 08:34:13 -0800

    module implementation of disjoint set

---
+++ 2018/DisjointSet.fs
@@ -0,0 +1,44 @@
+module DisjointSet
+
+type DisjointSet<'T when 'T : comparison> = {
+    ids : Map<'T, int>
+    parents : Map<int, int>
+    }
+
+let empty : DisjointSet<'T> =
+    { ids = Map.empty; parents = Map.empty }
+
+let add (x: 'T) (ds: DisjointSet<'T>) : DisjointSet<'T> =
+    let newId = 1 + Seq.length ds.ids
+    {
+        ids = Map.add x newId ds.ids
+        parents = Map.add newId newId ds.parents
+    }
+
+let ofList (xs: list<'T>) : DisjointSet<'T> =
+    {
+        ids = List.mapi (fun i x -> x, i) xs |> Map.ofList;
+        parents = List.mapi (fun i _ -> i, i) xs |> Map.ofList
+    }
+
+let private findRoot (x: 'T) (ds: DisjointSet<'T>) : int =
+    let rec loop i =
+        let i' = Map.find i ds.parents
+        if i' = i then i else loop i'
+    loop (Map.find x ds.ids)
+
+let private compressPath (x: 'T) (root: int) (ds: DisjointSet<'T>) : DisjointSet<'T> =
+    let parents' =
+        let rec loop acc i =
+            let i' = Map.find i ds.parents
+            let acc' = Map.add i root acc
+            if i' = i then acc' else loop acc' i'
+        loop ds.parents (Map.find x ds.ids)
+    let ids' = Map.add x root ds.ids
+    { ids = ids'; parents = parents' }
+
+let union (x: 'T) (y: 'T) (ds: DisjointSet<'T>): DisjointSet<'T> =
+    let xr = findRoot x ds
+    compressPath x xr ds |> compressPath y xr
+
+
--- 2018/day25.fs
+++ 2018/day25.fs
@@ -1,43 +1,6 @@
+// fsharpc DisjointSet.fs day25.fs -o day25.exe
 
-type UnionFind<'T when 'T : comparison> = {
-    ids : Map<'T, int>
-    parents : Map<int, int>
-    } with
-
-    static member empty : UnionFind<'T> =
-        { ids = Map.empty; parents = Map.empty }
-
-    member this.add (x: 'T) : UnionFind<'T> =
-        let newId = 1 + Seq.length this.ids
-        { ids = Map.add x newId this.ids;
-          parents = Map.ofList [newId, newId]
-          }
-
-    member private this.findRoot (x: 'T) : int * int =
-        let rec loop i n =
-            let i' = Map.find i this.parents
-            if i' = i then i, n else loop i' (n + 1)
-        loop (Map.find x this.ids) 0
-
-    member private this.compressPath (x: 'T) (root: int) : UnionFind<'T> =
-        let parents' =
-            let rec loop acc i =
-                let i' = Map.find i this.parents
-                let acc' = Map.add i root acc
-                if i' = i then acc' else loop acc' i'
-            loop this.parents (Map.find x this.ids)
-        let ids' = Map.add x root this.ids
-        { ids = ids'; parents = parents' }
-
-    member this.union (x: 'T) (y: 'T) : UnionFind<'T> =
-        let xr, xn = this.findRoot x
-        let yr, yn = this.findRoot y
-        let r = if xn > yn then xr else yr
-        (this.compressPath x r).compressPath y r
-
-    // Inefficient equivalence test.
-    member this.isEquivalentINEFFICIENT (x: 'T) (y: 'T) : bool =
-        fst (this.findRoot x) = fst (this.findRoot y)
+open DisjointSet
 
 let input : int [][] =
     System.IO.File.ReadAllLines("day25.txt")
