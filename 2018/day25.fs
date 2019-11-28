1d1ea7758ad44e529213aca5b7fd9c46767f7374
Maxwell Sayles <maxwellsayles@gmail.com>
2019-11-27 21:58:42 -0800

    easy once you have a disjoint-set

--- 2018/day25.fs
+++ 2018/day25.fs
@@ -7,8 +7,23 @@ let input : int [][] =
     |> Array.map (fun (s: string) -> s.Split([|','|]))
     |> Array.map (Array.map int)
 
+let distance (p1: int []) (p2: int []) : int =
+    Array.map2 (fun a b -> abs (a - b)) p1 p2
+    |> Array.sum
+
 [<EntryPoint>]
 let main args =
-    printfn "%A" input
+    let initSet = DisjointSet.ofList (List.ofSeq input)
+    let n = Array.length input
+    [ for i in [0 .. n - 2] do
+      for j in [i + 1 .. n - 1] do
+      let a = input.[i]
+      let b = input.[j]
+      if distance a b <= 3
+      then yield DisjointSet.makeEquivalent a b ]
+    |> List.fold (fun ds f -> f ds) initSet
+    |> DisjointSet.compressAll
+    |> DisjointSet.countEquivalenceClasses
+    |> printfn "%d"
 
     0
