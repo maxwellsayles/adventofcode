306408011104ed58e4c32ad9bd2141177c9cb6d5
Maxwell Sayles <maxwellsayles@gmail.com>
2019-12-02 22:03:10 -0800

    okay, so rust catches over/underflows, and has move types....

--- 2019/day01.rs
+++ 2019/day01.rs
@@ -1,17 +1,33 @@
 use std::fs;
 
-fn fuel(mass: u32) -> u32 {
+fn fuel(mass: i32) -> i32 {
     return mass / 3 - 2;
+}
+
+fn fuelr(mass: i32, acc: i32) -> i32 {
+    let f = mass / 3 - 2;
+    if f > 0 {
+        return fuelr(f, acc + f);
+    } else {
+        return acc;
+    }
 }
 
 fn main() {
     let contents = fs::read_to_string("day01.txt").expect("WTF");
     let lines = contents
         .lines()
-        .map(|x| x.parse::<u32>().expect("WTF"));
+        .map(|x| x.parse::<i32>().expect("WTF"));
 
-    let solve1: u32 = lines
+    let solve1: i32 = lines
+        .clone()
         .map(fuel)
         .sum();
     println!("{}", solve1);
+
+    let solve2: i32 = lines
+        .clone()
+        .map(|x| fuelr(x, 0))
+        .sum();
+    println!("{}", solve2);
 }
