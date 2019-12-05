05eff9fa2103a258dd6a3c0050ddb151fcd37ce9
Maxwell Sayles <maxwellsayles@gmail.com>
2019-12-04 21:37:43 -0800

    first attempt. wrongo

---
+++ 2019/day02.rs
@@ -0,0 +1,37 @@
+use std::fs;
+
+fn run(mut code: Vec<i32>, ip: usize) -> Vec<i32> {
+    let instr = code[ip];
+    if instr == 1 {
+        let x = code[ip + 1];
+        let y = code[ip + 2];
+        let t = code[ip + 3] as usize;
+        code[t] = x + y;
+    } else if instr == 2 {
+        let x = code[ip + 1];
+        let y = code[ip + 2];
+        let t = code[ip + 3] as usize;
+        code[t] = x * y;
+    } else if instr == 99 {
+        return code;
+    }
+    run(code, ip + 4)
+}
+
+fn solve1(mut code: Vec<i32>) -> i32 {
+    code[1] = 12;
+    code[2] = 2;
+    run(code, 0)[0]
+}
+
+fn main() {
+    let contents = fs::read_to_string("day02.txt")
+        .expect("WTF");
+
+    let input: Vec<i32> = contents
+        .split(',')
+        .map(|s| s.trim().parse::<i32>().expect("WTF"))
+        .collect();
+
+    println!("{}", solve1(input.clone()));
+}
