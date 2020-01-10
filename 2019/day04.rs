24712228e59f4319adfbfad3a41efb56b1f7d286
Maxwell Sayles <maxwellsayles@gmail.com>
2020-01-09 21:37:10 -0800

    I was on this mission for a DP solution, but brute force was just fine and so much easier. I might still come back and try the DP solution.

--- 2019/day04.rs
+++ 2019/day04.rs
@@ -1,51 +1,29 @@
-//const input: (i64, i64) = (356261, 846303);
-//const input1: (i64, i64) = (356666, 777777);
+const INPUT: (i64, i64) = (356261, 846303);
 
-fn get_counts(xs: &[usize; 6]) -> i64 {
-    let mut opt = [[0i64; 6]; 10];
-
-    for n in 0..=xs[0] {
-        opt[n][0] = 1 + n as i64;
-    }
-    for i in 1..=5 {
-        opt[0][i] = 1;
-    }
-    for n in 1..=xs[1] {        
-        let a = opt[n - 1][1];
-        let b = if n <= xs[0] { 1 } else { 0 };
-        let c = 0;
-        opt[n][1] = a + b + c;
-    }
-    for n in (xs[1] + 1)..=9 {
-        opt[n][1] = opt[n - 1][1];
-    }
-    for i in 2..=5 {
-        for n in 1..=xs[i] {
-            let a = opt[n - 1][i];
-            let b = if n <= xs[i - 1] {
-                opt[n][i - 2]
-            } else {
-                0
-            };
-            let c = opt[n - 1][i - 1];
-            opt[n][i] = a + b + c;
+fn is_valid(x: i64) -> bool {
+    let digits: Vec<_> = x.to_string()
+        .chars()
+        .map(|d| d.to_digit(10).unwrap())
+        .collect();
+    let n = digits.len();
+    let mut neighbor = false;
+    for i in 1..n {
+        if digits[i-1] > digits[i] {
+            return false;
         }
-        for n in (xs[i] + 1)..=9 {
-            opt[n][i] = opt[n - 1][i];
+        if digits[i-1] == digits[i] {
+            neighbor = true;
         }
     }
-    // for n in 0..=9 {
-    //     println!("{:?}", opt[n]);
-    // }
-    opt[9][5]
+    neighbor
 }
 
 fn main() {
-    let xs = [3, 5, 6, 6, 6, 6];
-    let x = get_counts(&xs);
-    println!("{}", x);
-    let ys = [8, 8, 8, 8, 8, 8];
-    let y = get_counts(&ys);
-    println!("{}", y);
-    println!("{}", y - x + 1);
+    let mut count: i64 = 0;
+    for x in INPUT.0..=INPUT.1 {
+        if is_valid(x) {
+            count += 1
+        }
+    }
+    println!("{}", count);
 }
