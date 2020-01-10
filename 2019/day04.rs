fb4759edc3a333639d62f8e176748355b0cf8b52
Maxwell Sayles <maxwellsayles@gmail.com>
2020-01-09 21:48:30 -0800

    not too tricky, but a little subtle with the possibility of a pair coming later than a triple

--- 2019/day04.rs
+++ 2019/day04.rs
@@ -18,10 +18,42 @@ fn is_valid(x: i64) -> bool {
     neighbor
 }
 
+fn is_valid2(x: i64) -> bool {
+    let digits: Vec<_> = x.to_string()
+        .chars()
+        .map(|d| d.to_digit(10).unwrap())
+        .collect();
+    let n = digits.len();
+    let mut neighbor = std::u32::MAX;
+    for i in 1..n {
+        if digits[i-1] > digits[i] {
+            return false;
+        }
+        if digits[i-1] == digits[i] && neighbor == std::u32::MAX {
+            neighbor = digits[i];
+        }
+        if i >= 2 &&
+            digits[i-1] == digits[i] &&
+            digits[i-2] == digits[i-1] &&
+            digits[i] == neighbor {
+                neighbor = std::u32::MAX;
+            }
+    }
+    neighbor != std::u32::MAX
+}
+
 fn main() {
     let mut count: i64 = 0;
     for x in INPUT.0..=INPUT.1 {
         if is_valid(x) {
+            count += 1
+        }
+    }
+    println!("{}", count);
+
+    count = 0;
+    for x in INPUT.0..=INPUT.1 {
+        if is_valid2(x) {
             count += 1
         }
     }
