e23d4129870203578eb5155fa1435ee5e3f66ec8
Maxwell Sayles <maxwellsayles@gmail.com>
2020-01-17 22:10:07 -0800

    this one was kind of fun and it let me play with rust lifetimes, which is something new

--- 2019/day06.rs
+++ 2019/day06.rs
@@ -1,3 +1,4 @@
+use std::cmp;
 use std::collections::HashMap;
 use std::fs;
 
@@ -10,6 +11,39 @@ fn depth(orbits: &HashMap<&str, &str>, node: &str) -> i64 {
     }
     d
 }
+
+fn compute_pair_depths<'a>(
+    orbits: &HashMap<&'a str, &'a str>,
+) -> HashMap<(&'a str, &'a str), i64> {
+    let mut res = HashMap::new();
+    let srcs = orbits.keys().map(|x| x.clone());
+    for src in srcs {
+        let mut d: i64 = 0;
+        let mut dst = src;
+        while dst != "COM" {
+            dst = orbits.get(dst).unwrap();
+            d += 1;
+            res.insert((src, dst), d);
+        }
+    }
+    res
+}
+
+fn distance_between(
+    pair_depths: &HashMap<(&str, &str), i64>,
+    src: &str,
+    dst: &str,
+) -> i64 {
+    let mut best = std::i64::MAX;
+    for ((a, b), dist_src) in pair_depths.iter() {
+        if a.clone() == src {
+            if let Some(dist_dst) = pair_depths.get(&(dst, b.clone())) {
+                best = cmp::min(best, dist_src + dist_dst - 2);
+            }
+        }
+    }
+    best
+}       
 
 fn main() {
     let contents = fs::read_to_string("day06.txt")
@@ -24,4 +58,8 @@ fn main() {
         .map(|k| depth(&input, k))
         .sum();
     println!("{}", solve1);
+
+    let pair_depths = compute_pair_depths(&input);
+    let d = distance_between(&pair_depths, "YOU", "SAN");
+    println!("{}", d);
 }
