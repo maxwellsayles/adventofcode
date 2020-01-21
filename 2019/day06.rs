a5b779de4553cc80bf4dae0b719f860d66ab799f
Maxwell Sayles <maxwellsayles@gmail.com>
2020-01-20 21:45:05 -0800

    just not thinking straight (or simply enough). just record depths from SAN back, and then find first collision from YOU back.

--- 2019/day06.rs
+++ 2019/day06.rs
@@ -1,4 +1,3 @@
-use std::cmp;
 use std::collections::HashMap;
 use std::fs;
 
@@ -12,39 +11,31 @@ fn depth(orbits: &HashMap<&str, &str>, node: &str) -> i64 {
     d
 }
 
-fn compute_depth<'a>(
-    orbits: &HashMap<&'a str, &'a str>,
-    depths: &mut HashMap<(&'a str, &'a str), i64>,
-    src: &'a str,
-) {
-    let mut d: i64 = 0;
-    let mut n = src;
-    while n != "COM" {
-        n = orbits.get(n).unwrap();
-        d += 1;
-        depths.insert((src, n), d);
-    }
-}
-
 fn distance_between(
     orbits: &HashMap<&str, &str>,
     src: &str,
     dst: &str,
 ) -> i64 {
     let mut depths = HashMap::new();
-    compute_depth(&orbits, &mut depths, "YOU");
-    compute_depth(&orbits, &mut depths, "SAN");
+    let mut d: i64 = 0;
+    let mut n = dst;
+    while n != "COM" {
+        n = orbits.get(n).unwrap();
+        d += 1;
+        depths.insert(n, d);
+    }
 
-    let mut best = std::i64::MAX;
-    for ((a, b), dist_src) in depths.iter() {
-        if *a == src {
-            if let Some(dist_dst) = depths.get(&(dst, *b)) {
-                best = cmp::min(best, dist_src + dist_dst - 2);
-            }
+    n = src;
+    d = 0;
+    while n != "COM" {
+        n = orbits.get(n).unwrap();
+        d += 1;
+        if let Some(dist) = depths.get(n) {
+            return d + dist - 2;
         }
     }
-    best
-}       
+    -1
+}      
 
 fn main() {
     let contents = fs::read_to_string("day06.txt")
