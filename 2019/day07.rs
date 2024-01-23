bc5f13e358171a10acc43688c11eb7d824f955ad
Maxwell Sayles <maxwellsayles@gmail.com>
2024-01-22 17:26:12 -0800

    Works with example programs and inputs.

--- 2019/day07.rs
+++ 2019/day07.rs
@@ -1,5 +1,5 @@
 use std::collections::VecDeque;
-use std::fs;
+// use std::fs;
 // use itertools::Itertools;
 
 struct IntcodeComputer {
@@ -97,9 +97,12 @@ impl IntcodeComputer {
     }
 
     fn run(&mut self) {
-	while !self.halted {
+	while self.outputs.len() == 0 {
 	    self.step();
 	}
+	// while !self.halted {
+	//     self.step();
+	// }
     }
 }
 
@@ -110,16 +113,36 @@ fn main() {
     // }
     // println!("Finished!");
 
-    let contents = fs::read_to_string("day05.txt")
-        .unwrap();
-    let code: Vec<i64> = contents
-        .split(',')
-        .map(|s| s.trim().parse::<i64>().unwrap())
-        .collect();
+    // let contents = fs::read_to_string("day05.txt")
+    //     .unwrap();
+    // let code: Vec<i64> = contents
+    //     .split(',')
+    //     .map(|s| s.trim().parse::<i64>().unwrap())
+    //     .collect();
 
-    let mut comp = IntcodeComputer::new(VecDeque::from(vec![5]), code);
-    comp.run();
-    for out in comp.outputs {
-	println!("{}", out);
+    // Expected output: 43210
+    // let code = vec![3,15,3,16,1002,16,10,16,1,16,15,15,4,15,99,0,0];
+    // let phase_sequence = vec![4, 3, 2, 1, 0];
+
+    // Expected output: 54321
+    // let code = vec![3,23,3,24,1002,24,10,24,1002,23,-1,23,
+    // 		    101,5,23,23,1,24,23,23,4,23,99,0,0];
+    // let phase_sequence = vec![0,1,2,3,4];
+
+    // Expected output: 65210
+    let code = vec![3,31,3,32,1002,32,10,32,1001,31,-2,31,1007,31,0,33,
+		    1002,33,7,33,1,33,31,31,1,32,31,31,4,31,99,0,0,0];
+    let phase_sequence = vec![1, 0, 4, 3, 2];
+
+    let mut output = 0;
+    for phase in phase_sequence {
+	let inputs = VecDeque::from(vec![phase, output]);
+	let mut comp = IntcodeComputer::new(inputs, code.clone());
+	comp.run();
+	output = match comp.outputs.pop_front() {
+	    Some(i) => i,
+	    None => panic!("Expected an output!"),
+	};
     }
+    println!("{}", output);
 }
