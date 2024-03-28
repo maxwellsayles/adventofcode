16288ef08323d14b138dcf84300f176de83f5441
Maxwell Sayles <maxwellsayles@gmail.com>
2024-03-27 17:07:09 -0700

    straightforward implementation for part 1.

--- 2019/day16.rs
+++ 2019/day16.rs
@@ -1,11 +1,11 @@
 use std::fs;
 
-struct FFTIter {
+struct FFTValIter {
     idx: usize,
     n: usize,
 }
 
-impl FFTIter {
+impl FFTValIter {
     fn new(n: usize) -> Self {
 	Self {
 	    idx: 0,
@@ -14,7 +14,7 @@ impl FFTIter {
     }
 }
 
-impl Iterator for FFTIter {
+impl Iterator for FFTValIter {
     type Item = i32;
 
     fn next(&mut self) -> Option<Self::Item> {
@@ -26,6 +26,36 @@ impl Iterator for FFTIter {
     }
 }
 
+fn dot_product<'a>(
+    x: impl Iterator<Item=&'a i32>,
+    y: impl Iterator<Item=i32>,
+) -> i32 {
+    x.zip(y).map(|(a, b)| a * b).sum()
+}
+
+// Least Significant Digit
+fn lsd(x: i32) -> i32 {
+    x.abs() % 10
+}
+
+fn fft(x: &Vec<i32>) -> Vec<i32> {
+    let mut res = Vec::with_capacity(x.len());
+    for i in 1..=x.len() {
+	let d = dot_product(x.iter(), FFTValIter::new(i));
+	res.push(lsd(d));
+    }
+    res
+}
+
+fn part1(xs: &Vec<i32>) {
+    let mut ys = xs.clone();
+    for _ in 0..100 {
+	ys = fft(&ys);
+    }
+    let res = ys.iter().take(8).map(|x| x.to_string()).collect::<Vec<_>>().join("");
+    println!("{}", res);
+}
+
 fn main() {
     let contents: Vec<i32> = fs::read_to_string("day16.txt")
 	.unwrap()
@@ -34,8 +64,5 @@ fn main() {
 	.map(|c| c.to_digit(10).unwrap() as i32)
 	.collect();
 
-    let fft = FFTIter::new(2);
-    for x in fft.take(8) {
-	println!("{}", x);
-    }
+    part1(&contents);
 }
