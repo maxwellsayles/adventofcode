0278c01ef3cdea3ae42101fb28a97b245885afc7
Maxwell Sayles <maxwellsayles@gmail.com>
2024-10-29 16:08:50 -0700

    Just a little logic to think about when to jump and when not to jump.

--- 2019/day21.rs
+++ 2019/day21.rs
@@ -1,3 +1,6 @@
+// Little help from:
+// https://cutonbuminband.github.io/AOC/qmd/2019.html#day-21-springdroid-adventure
+
 use std::fs;
 
 use crate::intcode_computer::IntcodeComputer;
@@ -35,6 +38,32 @@ fn part1(code: &Vec<i64>) {
     }
 }
 
+fn part2(code: &Vec<i64>) {
+    let mut comp = IntcodeComputer::new(vec![], &code);
+
+    // Jump if B or C is a hole and D and H are not.
+    // Always jump if A is a hole.
+    let prog = vec![
+	"NOT B J",
+	"NOT C T",
+	"OR T J",
+	"AND D J",
+	"AND H J",
+	"NOT A T",
+	"OR T J",
+    ];
+    for p in prog.iter() {
+	input_string(&mut comp, p);
+    }
+    input_string(&mut comp, "RUN");
+    comp.run();
+    while let Some(c) = comp.remove_output() {
+	if c > 255 {
+	    println!("{}", c);
+	}
+    }
+}
+
 fn main() {
     let code = fs::read_to_string("day21.txt")
 	.unwrap()
@@ -42,4 +71,5 @@ fn main() {
         .map(|s| s.trim().parse::<i64>().unwrap())
         .collect();
     part1(&code);
+    part2(&code);
 }
