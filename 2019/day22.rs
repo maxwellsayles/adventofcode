099d33ef9017567b7af52bebf9c297a6fbc72918
Maxwell Sayles <maxwellsayles@gmail.com>
2024-11-03 16:06:49 -0800

    Generalize the solution by encoding the modular arithmetic as an offset and delta, i.e. you can compute loc(k) = i + j * k mod p

--- 2019/day22.rs
+++ 2019/day22.rs
@@ -1,7 +1,7 @@
 use regex::Regex;
 use std::fs;
 
-fn process_input(filename: &str, mut i: i32, p: i32) -> i32 {
+fn process_input(filename: &str, p: i32) -> (i32, i32) {
     let content = fs::read_to_string(filename)
 	.unwrap();
     let re_deal_into_new_stack =
@@ -10,26 +10,35 @@ fn process_input(filename: &str, mut i: i32, p: i32) -> i32 {
 	Regex::new(r"^cut (-?\d+)$").unwrap();
     let re_deal_with_increment =
 	Regex::new(r"^deal with increment (\d+)$").unwrap();
+    let mut i = 0;
+    let mut j = 1;
     for l in content.lines() {
 	if let Some(_) = re_deal_into_new_stack.captures(l) {
 	    i = p - 1 - i;
+	    j = p - 1 - j;
 	} else if let Some(captures) = re_cut.captures(l) {
 	    let n = captures.get(1).unwrap().as_str().parse::<i32>().unwrap();
 	    i = (i - n) % p;
 	    if i < 0 {
 		i += p;
 	    }
+	    j = (j - n) % p;
+	    if j < 0 {
+		j += p;
+	    }
 	} else if let Some(captures) = re_deal_with_increment.captures(l) {
 	    let n = captures.get(1).unwrap().as_str().parse::<i32>().unwrap();
 	    i = (i * n) % p;
+	    j = (j * n) % p;
 	} else {
 	    panic!("Unrecognized instructions: {}", l);
 	}
     }
-    i
+    (i, (j - i) % p)
 }
 
 fn main() {
-    let i = process_input("day22.txt", 2019, 10007);
-    println!("{}", i);
+    let (i, j) = process_input("day22.txt", 10007);
+    let k = ((j * 2019) + i) % 10007;
+    println!("{}", k);
 }
