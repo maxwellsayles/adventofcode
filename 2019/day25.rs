4038fcea50136821f273d9614a7585de4426adc3
Maxwell Sayles <maxwellsayles@gmail.com>
2024-11-14 10:45:44 -0800

    DFS of the maze and pick up items. We are too heavy when we get to the pressure sensitive floor.

--- 2019/day25.rs
+++ 2019/day25.rs
@@ -1,3 +1,5 @@
+use regex::Regex;
+use std::collections::{ HashSet };
 use std::env;
 use std::fs;
 use std::io;
@@ -6,25 +8,129 @@ use crate::intcode_computer::IntcodeComputer;
 
 mod intcode_computer;
 
-fn repl(code: &Vec<i64>) {
-    let mut comp = IntcodeComputer::new(vec![], &code);
-    loop {
-	comp.run();
-	while let Some(c) = comp.remove_output() {
-	    print!("{}", (c as u8) as char);
-	}
-	let mut buffer = String::new();
-	io::stdin().read_line(&mut buffer).unwrap();
-	for c in buffer.chars() {
-	    comp.add_input(c as i64);
-	}
-    }
-}
-
 const HELP: &str = "day25 without any arguments will run the solutions.
 Options:
 \t-r\tRun game inside the REPL.
 ";
+
+fn to_comp(comp: &mut IntcodeComputer, buff: &String) {
+    for c in buff.chars() {
+	comp.add_input(c as i64);
+    }
+}
+
+fn from_comp(comp: &mut IntcodeComputer) -> String {
+    let mut buffer = String::new();
+    while let Some(c) = comp.remove_output() {
+	buffer.push((c as u8) as char);
+    }
+    buffer
+}
+
+fn repl(code: &Vec<i64>) {
+    let mut comp = IntcodeComputer::new(vec![], &code);
+    loop {
+	comp.run();
+	print!("{}", from_comp(&mut comp));
+	let mut buffer = String::new();
+	io::stdin().read_line(&mut buffer).unwrap();
+	to_comp(&mut comp, &buffer);
+    }
+}
+
+struct State {
+    comp: IntcodeComputer,
+    pos: (i32, i32),
+    visited: HashSet<(i32, i32)>,
+    ignored_items: HashSet<String>,
+}
+
+impl State {
+
+    fn step_comp(&mut self, input: &str) -> String {
+	println!("{}", input);
+	if input.len() > 0 {
+	    to_comp(&mut self.comp, &(String::from(input) + &"\n"));
+	}
+	self.comp.run();
+	let buff = from_comp(&mut self.comp);
+	print!("{}", buff);
+	buff
+    }
+
+    fn try_move(&mut self, dir: &str, dx: i32, dy: i32) -> bool {
+	let buff = self.step_comp(dir);
+
+	// Test if move was successful.
+	let re_fail = Regex::new(r"You can't go that way.").unwrap();
+	if re_fail.is_match(buff.as_str()) {
+	    return false
+	}
+
+	// Take item if there is an item.
+	let re_items = Regex::new(r"Items here:\n- (.*)\n").unwrap();
+	match re_items.captures(buff.as_str()) {
+	    Some(captures) => {
+		let item = captures.get(1).unwrap().as_str();
+		if !self.ignored_items.contains(&String::from(item)) {
+		    self.step_comp(&(String::from("take ") + item).as_str());
+		}
+	    },
+	    None => {},
+	}
+
+	self.visited.insert(self.pos.clone());
+	self.pos = (self.pos.0 + dx, self.pos.1 + dy);
+	true
+    }
+
+    fn step(&mut self) {
+	if self.visited.contains(&self.pos) {
+	    return;
+	}
+
+	if self.try_move("north", 0, -1) {
+	    self.step();
+	    assert!(self.try_move("south", 0, 1));
+	}
+	if self.try_move("east", 1, 0) {
+	    self.step();
+	    assert!(self.try_move("west", -1, 0));
+	}
+	if self.try_move("south", 0, 1) {
+	    self.step();
+	    assert!(self.try_move("north", 0, -1));
+	}
+	if self.try_move("west", -1, 0) {
+	    self.step();
+	    assert!(self.try_move("east", 1, 0));
+	}
+    }
+
+    fn run(&mut self) {
+	// Run one step and get output. Then iterate commands.
+	self.step_comp(&String::new());
+	self.step();
+    }
+}
+
+fn part1(code: &Vec<i64>) {
+    let comp = IntcodeComputer::new(vec![], &code);
+    let ignored_items = HashSet::from([
+	"photons",
+	"infinite loop",
+	"escape pod",
+	"molten lava",
+	"giant electromagnet",
+    ].map(|i| String::from(i)));
+    let mut state = State {
+	comp,
+	pos: (0, 0),
+	visited: HashSet::new(),
+	ignored_items,
+    };
+    state.run();
+}
 
 fn main() {
     let code = fs::read_to_string("day25.txt")
@@ -34,10 +140,13 @@ fn main() {
         .collect();
 
     let args: Vec<String> = env::args().collect();
-    if args.len() > 1 {
-	match args[1].as_str() {
+    match args.len() {
+	1 => part1(&code),
+	2 => match args[1].as_str() {
 	    "-r" => repl(&code),
 	    _ => print!("{}", HELP),
-	}
+	},
+	_ => print!("{}", HELP),
     }
 }
+
