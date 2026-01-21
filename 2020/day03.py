d189abe08a2807dd9da097b533bc6eb841182c13
Maxwell Sayles <maxwellsayles@gmail.com>
2026-01-20 18:31:09 -0800

    part2 of day3

--- 2020/day03.py
+++ 2020/day03.py
@@ -1,6 +1,8 @@
+import math
+
 from pathlib import Path
 
-def part1(input):
+def traverse(input, dx, dy):
     x, y = 0, 0
     h = len(input)
     w = len(input[0])
@@ -8,10 +10,22 @@ def part1(input):
     while y < h:
         if input[y][x] == '#':
             c += 1
-        x = (x + 3) % w
-        y += 1
+        x = (x + dx) % w
+        y += dy
     return c
+
+def part1(input):
+    return traverse(input, 3, 1)
+
+def part2(input):
+    return math.prod([traverse(input, 1, 1),
+                      traverse(input, 3, 1),
+                      traverse(input, 5, 1),
+                      traverse(input, 7, 1),
+                      traverse(input, 1, 2),
+                      ])
 
 if __name__ == '__main__':
     input = Path('day03.txt').read_text().splitlines()
     print(part1(input))
+    print(part2(input))
