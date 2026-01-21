04d8b2928baaea9bf7c9659f91a3dba70f16aa54
Maxwell Sayles <maxwellsayles@gmail.com>
2026-01-20 18:10:42 -0800

    part1 of day2

---
+++ 2020/day02.py
@@ -0,0 +1,23 @@
+import re
+
+def parse(line):
+    pattern = r"(\d+)-(\d+) (.): (.*)"
+    match = re.match(pattern, line)
+    if match:
+        return (int(match.group(1)),
+                int(match.group(2)),
+                match.group(3),
+                match.group(4))
+    return None
+
+def is_valid(line):
+    x, y, c, s = line
+    return x <= s.count(c) <= y
+
+def part1(input):
+    # return len(list(filter(is_valid, input)))
+    return sum(map(is_valid, input))
+
+if __name__ == '__main__':
+    input = [parse(l) for l in open('day02.txt')]
+    print(part1(input))
