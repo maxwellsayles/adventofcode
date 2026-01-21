1175aab730967f52924367d470ba243c12488ec7
Maxwell Sayles <maxwellsayles@gmail.com>
2026-01-20 17:47:25 -0800

    Day1 Part2

--- 2020/day01.py
+++ 2020/day01.py
@@ -1,10 +1,29 @@
+T = 2020
+
 def part1(input):
-    h = {2020 - x: x for x in input}
+    h = {T - x: x for x in input}
     for x in input:
         if x in h:
             return x * h[x]
+    return None
+
+def part2(input):
+    xs = sorted(input)
+    for k, x in enumerate(xs):
+        i = 0
+        j = len(xs) - 1
+        while i != j:
+            s = x + xs[i] + xs[j]
+            if s == T and i != k and j != k:
+                return x * xs[i] * xs[j]
+            if s > T:
+                j -= 1
+            if s < T:
+                i += 1
+    return None
 
 if __name__ == '__main__':
     input = [int(x) for x in open("day01.txt")]
     print(part1(input))
+    print(part2(input))
 
