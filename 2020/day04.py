d10c3ffae4dc5190daea1f46f0458ba47b77b28e
Maxwell Sayles <maxwellsayles@gmail.com>
2026-01-22 15:19:21 -0800

    part2 of day4

--- 2020/day04.py
+++ 2020/day04.py
@@ -39,7 +39,7 @@ def parse_key_value_file(filepath):
 
     return data
 
-def part1(input):
+def has_required_keys(d):
     required = {
         'byr',
         'iyr',
@@ -50,9 +50,51 @@ def part1(input):
         'pid',
         # 'cid', # cid is optional
     }
-    return sum(map(lambda grp: required.issubset(grp), input))
+    return required.issubset(d)
+
+def part1(input):
+    return sum(map(has_required_keys, input))
+
+def maybe_int(value):
+    try:
+        return int(value)
+    except (ValueError, TypeError):
+        return None
+
+import re
+
+def is_valid_hgt(hgt):
+    match = re.fullmatch(r'(\d+)(cm|in)', hgt)
+    if not match:
+        return False
+
+    num, unit = int(match.group(1)), match.group(2)
+    return (unit == 'cm' and 150 <= num <= 193) or \
+           (unit == 'in' and 59 <= num <= 76)
+
+def part2(input):
+    def is_valid(d):
+        byr = maybe_int(d.get('byr')) or 0
+        iyr = maybe_int(d.get('iyr')) or 0
+        eyr = maybe_int(d.get('eyr')) or 0
+        hgt = d.get('hgt')
+        hcl = d.get('hcl')
+        ecl = d.get('ecl')
+        pid = d.get('pid')
+
+        return has_required_keys(d) and \
+            1920 <= byr <= 2002 and \
+            2010 <= iyr <= 2020 and \
+            2020 <= eyr <= 2030 and \
+            is_valid_hgt(hgt) and \
+            bool(re.fullmatch(r'#[0-9a-f]{6}', hcl)) and \
+            ecl in {'amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth'} and \
+            len(pid) == 9 and pid.isdigit()
+
+    return sum(map(is_valid, input))
 
 if __name__ == '__main__':
     input = parse_key_value_file('day04.txt')
     print(part1(input))
+    print(part2(input))
 
