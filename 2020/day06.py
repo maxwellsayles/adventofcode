466dcca81e84ad853a3f062faddb7f35efa20ebc
Maxwell Sayles <maxwellsayles@gmail.com>
2026-01-30 20:19:10 -0800

    Intersection and union <shrug />

--- 2020/day06.py
+++ 2020/day06.py
@@ -3,12 +3,19 @@ def parse_groups(filename):
         content = f.read().strip()
         return [block.splitlines() for block in content.split('\n\n')]
 
-def count_group(grp):
+def count_group1(grp):
     return len(set.union(*map(set, grp)))
 
 def part1(input):
-    return sum(map(count_group, input))
+    return sum(map(count_group1, input))
+
+def count_group2(grp):
+    return len(set.intersection(*map(set, grp)))
+
+def part2(input):
+    return sum(map(count_group2, input))
 
 if __name__ == '__main__':
     input = parse_groups('day06.txt')
     print(part1(input))
+    print(part2(input))
