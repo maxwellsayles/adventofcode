2d68f8495efd10db0bcf1edb1e9e3335c8680098
Maxwell Sayles <maxwellsayles@gmail.com>
2026-01-30 19:25:34 -0800

    Use the min and max, since that is all that we used sorting for anyway.

--- 2020/day05.py
+++ 2020/day05.py
@@ -8,9 +8,9 @@ def part1(input):
     return max(map(seatID, input))
 
 def part2(input):
-    ids = sorted(map(seatID, input))
-    x = ids[0]
-    y = ids[-1]
+    ids = list(map(seatID, input))
+    x = min(ids)
+    y = max(ids)
     h = y * (y + 1) // 2
     l = x * (x - 1) // 2
     return h - l - sum(ids)
