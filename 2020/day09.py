e0fedd14bb60cfcfb63fa065e7009e46f11cf352
Maxwell Sayles <maxwellsayles@gmail.com>
2026-03-16 12:58:26 -0700

    part 2 of day 9. Just iterate all pairs of prefix sums to find the target

--- 2020/day09.py
+++ 2020/day09.py
@@ -1,3 +1,5 @@
+import itertools
+
 def is2sum(active, t):
     active = set(active)
     for x in active:
@@ -14,6 +16,21 @@ def part1(input):
         i += 1
     return input[i]
 
+def part2(input, t):
+    sums = list(itertools.accumulate(input))
+    n = len(input)
+    for i in range(n):
+        for j in range(i + 1, n):
+            s = sums[j] - sums[i]
+            if s == t:
+                sub = input[i : j + 1]
+                return min(sub) + max(sub)
+            elif s > t:
+                break
+    return None
+
 if __name__ == '__main__':
     input = [int(x) for x in open("day09.txt")]
-    print(part1(input))
+    t = part1(input)
+    print(t)
+    print(part2(input, t))
