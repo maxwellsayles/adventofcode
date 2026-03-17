6807868ce641a9295f0d8d99816f5d3b1a5903db
Maxwell Sayles <maxwellsayles@gmail.com>
2026-03-16 17:40:49 -0700

    part 2 of day 10. A little DP. Only really need the last three elements of opts.

--- 2020/day10.py
+++ 2020/day10.py
@@ -1,6 +1,4 @@
 def part1(input):
-    input = sorted(input)
-    input.insert(0, 0) # prepend a 0 for the outlet
     a, b = 0, 0
     for x, y in zip(input, input[1:]):
         d = y - x
@@ -9,8 +7,23 @@ def part1(input):
                 a += 1
             case 3:
                 b += 1
-    return a * (b + 1) # + 1 for the device itself
+    return a * b
+
+def part2(input):
+    opts = [1]
+    n = len(input)
+    for i in range(1, n):
+        opt = 0
+        for j in [i - 1, i - 2, i - 3]:
+            if j >= 0 and input[j] >= input[i] - 3:
+                opt += opts[j]
+        opts.append(opt)
+    return opts[-1]
 
 if __name__ == '__main__':
     input = [int(x) for x in open("day10.txt")]
+    input.sort()
+    input.insert(0, 0) # prepend a 0 for the outlet
+    input.append(input[-1] + 3) # the device is 3 greater than the highest
     print(part1(input))
+    print(part2(input))
